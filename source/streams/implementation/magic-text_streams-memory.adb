57722a08a53c85f13886bfce1365e4b88464a643
Vadim Godunko <godunko@adacore.com>
2020-03-30 23:37:20 +0300

    Minimal implementation of the memory based text stream.

---
+++ source/streams/implementation/magic-text_streams-memory.adb
@@ -0,0 +1,88 @@
+------------------------------------------------------------------------------
+--                         Language Server Protocol                         --
+--                                                                          --
+--                       Copyright (C) 2020, AdaCore                        --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Streams;
+
+with Magic.Unicode;
+
+package body Magic.Text_Streams.Memory is
+
+   ---------
+   -- Put --
+   ---------
+
+   procedure Put
+     (Self    : in out Memory_UTF8_Output_Stream;
+      Item    : Magic.Characters.Magic_Character;
+      Success : in out Boolean)
+   is
+      use type Ada.Streams.Stream_Element;
+      use type Magic.Unicode.Code_Point;
+
+      Code : constant Magic.Unicode.Code_Point :=
+        Magic.Characters.Magic_Character'Pos (Item);
+
+   begin
+      case Code is
+         when 16#00_0000# .. 16#00_007F# =>
+            Self.Buffer.Append (Ada.Streams.Stream_Element (Code));
+
+         when 16#00_0080# .. 16#00_07FF# =>
+            Self.Buffer.Append
+              (2#1100_0000#
+               or Ada.Streams.Stream_Element
+                 ((Code and 2#111_1100_0000#) / 2#100_0000#));
+            Self.Buffer.Append
+              (2#1000_0000#
+               or Ada.Streams.Stream_Element
+                  (Code and 2#000_0011_1111#));
+
+         when 16#00_0800# .. 16#00_FFFF# =>
+            Self.Buffer.Append
+              (2#1110_0000#
+               or Ada.Streams.Stream_Element
+                 ((Code and 2#1111_0000_0000_0000#) / 2#1_0000_0000_0000#));
+            Self.Buffer.Append
+              (2#1000_0000#
+               or Ada.Streams.Stream_Element
+                 ((Code and 2#0000_1111_1100_0000#) / 2#100_0000#));
+            Self.Buffer.Append
+              (2#1000_0000#
+               or Ada.Streams.Stream_Element
+                  (Code and 2#0000_0000_0011_1111#));
+
+         when 16#01_0000# .. 16#10_FFFF# =>
+            Self.Buffer.Append
+              (2#1111_0000#
+               or Ada.Streams.Stream_Element
+                 ((Code and 2#1_1100_0000_0000_0000_0000#) / 2#100_0000_0000_0000_0000#));
+            Self.Buffer.Append
+              (2#1000_0000#
+               or Ada.Streams.Stream_Element
+                 ((Code and 2#0_0011_1111_0000_0000_0000#) / 2#1_0000_0000_0000#));
+            Self.Buffer.Append
+              (2#1000_0000#
+               or Ada.Streams.Stream_Element
+                 ((Code and 2#0_0000_0000_1111_1100_0000#) / 2#100_0000#));
+            Self.Buffer.Append
+              (2#1000_0000#
+               or Ada.Streams.Stream_Element
+                  (Code and 2#0_0000_0000_0000_0011_1111#));
+      end case;
+   end Put;
+
+end Magic.Text_Streams.Memory;

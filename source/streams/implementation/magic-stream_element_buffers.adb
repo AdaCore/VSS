2ef1feb3f815335580efa124aeca53633ef0181b
Vadim Godunko <godunko@adacore.com>
2020-03-30 22:33:12 +0300

    Minimal implementation of the stream element buffer.

---
+++ source/streams/implementation/magic-stream_element_buffers.adb
@@ -0,0 +1,120 @@
+------------------------------------------------------------------------------
+--                         Language Server Protocol                         --
+--                                                                          --
+--                       Copyright (C) 2020, AdaCore                        --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Unchecked_Deallocation;
+
+package body Magic.Stream_Element_Buffers is
+
+   ------------
+   -- Adjust --
+   ------------
+
+   overriding procedure Adjust (Self : in out Stream_Element_Buffer) is
+      Source : constant Data_Access := Self.Data;
+
+   begin
+      if Source /= null then
+         Self.Data :=
+           new Data_Record
+             (Ada.Streams.Stream_Element_Offset'Max
+                (Source.Size, Self.Capacity));
+
+         Self.Data.Length := Source.Length;
+         Self.Data.Storage (Source.Storage'Range) := Source.Storage;
+      end if;
+   end Adjust;
+
+   ------------
+   -- Append --
+   ------------
+
+   procedure Append
+     (Self : in out Stream_Element_Buffer;
+      Item : Ada.Streams.Stream_Element)
+   is
+      use type Ada.Streams.Stream_Element_Offset;
+
+      Source : Data_Access := Self.Data;
+
+   begin
+      if Self.Data = null then
+         Self.Data := new Data_Record (1);
+         Self.Data.Length := 0;
+
+      elsif Self.Data.Size <= Self.Data.Length then
+         Self.Data :=
+           new Data_Record
+             (Ada.Streams.Stream_Element_Offset'Max
+                (Source.Size * 2, Self.Capacity));
+         Self.Data.Length := Source.Length;
+      end if;
+
+      Self.Data.Length := Self.Data.Length + 1;
+      Self.Data.Storage (Self.Data.Length) := Item;
+   end Append;
+
+   --------------
+   -- Finalize --
+   --------------
+
+   overriding procedure Finalize (Self : in out Stream_Element_Buffer) is
+      procedure Free is
+        new Ada.Unchecked_Deallocation (Data_Record, Data_Access);
+
+   begin
+      if Self.Data /= null then
+         Free (Self.Data);
+      end if;
+   end Finalize;
+
+   ----------
+   -- Read --
+   ----------
+
+   procedure Read
+     (Stream : not null access Ada.Streams.Root_Stream_Type'Class;
+      Item   : out Stream_Element_Buffer) is
+   begin
+      null;
+   end Read;
+
+   ------------------
+   -- Set_Capacity --
+   ------------------
+
+   procedure Set_Capacity
+     (Self     : in out Stream_Element_Buffer'Class;
+      Capacity : Ada.Streams.Stream_Element_Count) is
+   begin
+      Self.Capacity := Capacity;
+   end Set_Capacity;
+
+   -----------
+   -- Write --
+   -----------
+
+   procedure Write
+     (Stream : not null access Ada.Streams.Root_Stream_Type'Class;
+      Item   : Stream_Element_Buffer) is
+   begin
+      if Item.Data /= null then
+         Ada.Streams.Stream_Element_Array'Write
+           (Stream, Item.Data.Storage (1 .. Item.Data.Length));
+      end if;
+   end Write;
+
+end Magic.Stream_Element_Buffers;
---
+++ source/streams/magic-stream_element_buffers.ads
@@ -0,0 +1,77 @@
+------------------------------------------------------------------------------
+--                         Language Server Protocol                         --
+--                                                                          --
+--                       Copyright (C) 2020, AdaCore                        --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+private with Ada.Finalization;
+with Ada.Streams;
+
+package Magic.Stream_Element_Buffers is
+
+   pragma Preelaborate;
+   pragma Remote_Types;
+
+   type Stream_Element_Buffer is tagged private;
+
+   procedure Set_Capacity
+     (Self     : in out Stream_Element_Buffer'Class;
+      Capacity : Ada.Streams.Stream_Element_Count);
+   --  Request to preallocate memory to store given number of stream elements.
+
+   procedure Append
+     (Self : in out Stream_Element_Buffer;
+      Item : Ada.Streams.Stream_Element);
+   --  Append stream element to the end of the buffer
+
+private
+
+   type Data_Record (Size : Ada.Streams.Stream_Element_Count) is record
+      Length  : Ada.Streams.Stream_Element_Count;
+      Storage : Ada.Streams.Stream_Element_Array (1 .. Size);
+   end record;
+
+   type Data_Access is access all Data_Record;
+
+   procedure Read
+     (Stream : not null access Ada.Streams.Root_Stream_Type'Class;
+      Item   : out Stream_Element_Buffer);
+
+   procedure Write
+     (Stream : not null access Ada.Streams.Root_Stream_Type'Class;
+      Item   : Stream_Element_Buffer);
+
+   --  function Input
+   --    (Stream : not null access Ada.Streams.Root_Stream_Type'Class)
+   --     return Stream_Element_Buffer;
+   --
+   --  procedure Output
+   --    (Stream : not null access Ada.Streams.Root_Stream_Type'Class;
+   --     Item   : Stream_Element_Buffer);
+
+   type Stream_Element_Buffer is new Ada.Finalization.Controlled with record
+      Data     : Data_Access;
+      Capacity : Ada.Streams.Stream_Element_Count := 0;
+   end record;
+
+   for Stream_Element_Buffer'Read use Read;
+   for Stream_Element_Buffer'Write use Write;
+   --  for Stream_Element_Buffer'Input use Input;
+   --  for Stream_Element_Buffer'Output use Output;
+
+   overriding procedure Adjust (Self : in out Stream_Element_Buffer);
+
+   overriding procedure Finalize (Self : in out Stream_Element_Buffer);
+
+end Magic.Stream_Element_Buffers;

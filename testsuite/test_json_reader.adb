3d77a9ab9125c0c6770fdd39889fd8c213d844f7
Vadim Godunko <godunko@adacore.com>
2020-04-17 21:09:57 +0300

    Test driver for JSON_Simple_Reader.

---
+++ testsuite/test_json_reader.adb
@@ -0,0 +1,89 @@
+
+with Ada.Command_Line;
+with Ada.Streams.Stream_IO;
+with Ada.Text_IO;
+
+with Magic.JSON.Streams.Readers.Simple;
+
+with Memory_Text_Streams;
+
+procedure Test_JSON_Reader is
+
+   use all type Magic.JSON.Streams.Readers.JSON_Event_Kind;
+   use all type Magic.JSON.Streams.Readers.JSON_Reader_Error;
+
+   Input  : aliased Memory_Text_Streams.Memory_UTF8_Input_Stream;
+   Reader : Magic.JSON.Streams.Readers.Simple.JSON_Simple_Reader;
+   Count  : Natural := 0;
+
+begin
+   if Ada.Command_Line.Argument_Count /= 2 then
+      raise Program_Error with "incorrect command line arguments";
+   end if;
+
+   if Ada.Command_Line.Argument (1) = "s" then
+      Input.Set_Incremental (False);
+
+   elsif Ada.Command_Line.Argument (1) = "i" then
+      Input.Set_Incremental (True);
+
+   else
+      raise Program_Error with "incorrect parsing mode";
+   end if;
+
+   declare
+      File : Ada.Streams.Stream_IO.File_Type;
+      Aux  : Ada.Streams.Stream_Element_Array (1 .. 1_024);
+      Last : Ada.Streams.Stream_Element_Offset;
+
+   begin
+      Ada.Streams.Stream_IO.Open
+        (File, Ada.Streams.Stream_IO.In_File, Ada.Command_Line.Argument (2));
+
+      while not Ada.Streams.Stream_IO.End_Of_File (File) loop
+         Ada.Streams.Stream_IO.Read (File, Aux, Last);
+
+         for J in Aux'First .. Last loop
+            Input.Buffer.Append (Aux (J));
+         end loop;
+      end loop;
+
+      Ada.Streams.Stream_IO.Close (File);
+   end;
+
+   Reader.Set_Stream (Input'Unchecked_Access);
+
+   while Reader.Read_Next /= No_Token loop
+      case Reader.Event_Kind is
+         when Invalid =>
+            if Reader.Error /= Premature_End_Of_Document then
+               Ada.Text_IO.Put_Line
+                 (Magic.JSON.Streams.Readers.JSON_Event_Kind'Image
+                    (Reader.Event_Kind));
+
+               if Reader.Error = Not_Valid then
+                  Ada.Command_Line.Set_Exit_Status (1);
+
+                  return;
+
+               else
+                  raise Program_Error;
+               end if;
+
+            else
+               Count := Count + 1;
+
+               if Count > 1_000 then
+                  raise Program_Error;
+               end if;
+            end if;
+
+         when others =>
+            Count := 0;
+
+            Ada.Text_IO.Put_Line
+              (Magic.JSON.Streams.Readers.JSON_Event_Kind'Image
+                 (Reader.Event_Kind));
+      end case;
+   end loop;
+end Test_JSON_Reader;

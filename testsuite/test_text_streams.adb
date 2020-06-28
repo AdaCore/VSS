2497f644890bccb67ed521f10e99c631ba3b15a1
Vadim Godunko <godunko@adacore.com>
2020-06-28 22:56:01 +0300

    Test for text streams.

--- testsuite/test_text_streams.adb
+++ testsuite/test_text_streams.adb
@@ -21,28 +21,47 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
-with "gnatcoll_text";
+with Ada.Streams;
 
-project GNATCOLL_Text_Tests is
+with VSS.Text_Streams.Memory;
 
-   for Languages use ("Ada");
-   for Object_Dir use "../.objs/tests";
-   for Source_Dirs use ("../testsuite");
-   for Main use ("test_conversions.adb",
-                 "test_character_iterators.adb",
-                 "test_json_reader.adb",
-                 "test_json_writer.adb",
-                 "test_stream_element_buffer.adb",
-                 "test_string_equal",
-                 "test_text_streams");
+procedure Test_Text_Streams is
 
-   package Compiler is
-      for Switches ("Ada") use ("-g", "-O2", "-gnatW8");
-      for Switches ("hello_world_data.adb") use ("-g", "-O2");
-   end Compiler;
+   use type Ada.Streams.Stream_Element;
+   use type Ada.Streams.Stream_Element_Offset;
 
-   package Binder is
-      for Switches ("Ada") use ("-Wb");
-   end Binder;
+   Stream  : VSS.Text_Streams.Memory.Memory_UTF8_Output_Stream;
+   Success : Boolean := True;
 
-end GNATCOLL_Text_Tests;
+   Expected : constant Ada.Streams.Stream_Element_Array :=
+     (1  => 16#41#,
+      2  => 16#D0#,
+      3  => 16#91#,
+      4  => 16#E0#,
+      5  => 16#A4#,
+      6  => 16#95#,
+      7  => 16#F0#,
+      8  => 16#90#,
+      9  => 16#8C#,
+      10 => 16#88#);
+
+begin
+   Stream.Put ('A', Success);
+   Stream.Put ('Б', Success);
+   Stream.Put ('क', Success);
+   Stream.Put ('𐌈', Success);
+
+   if not Success then
+      raise Program_Error;
+   end if;
+
+   if Stream.Buffer.Length /= Expected'Length then
+      raise Program_Error;
+   end if;
+
+   for J in Expected'Range loop
+      if Stream.Buffer.Element (J) /= Expected (J) then
+         raise Program_Error;
+      end if;
+   end loop;
+end Test_Text_Streams;

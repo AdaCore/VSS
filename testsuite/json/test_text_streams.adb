6870fafdb63b9d76b22a8206cd7c50ca36d1a62a
Vadim Godunko <godunko@adacore.com>
2020-11-30 15:09:47 +0300

    Reuse code to decode UTF-8.

--- testsuite/json/test_text_streams.adb
+++ testsuite/json/test_text_streams.adb
@@ -1,3 +1,5 @@
+--  with Ada.Text_IO;
+--  with Ada.Wide_Wide_Text_IO;
 ------------------------------------------------------------------------------
 --                        M A G I C   R U N T I M E                         --
 --                                                                          --
@@ -48,8 +50,6 @@ package body Test_Text_Streams is
       Item    : out VSS.Characters.Virtual_Character;
       Success : in out Boolean)
    is
-      use type VSS.Unicode.Code_Point;
-
       procedure Report_Error (Message : String);
 
       ------------------
@@ -64,19 +64,10 @@ package body Test_Text_Streams is
          Self.Diagnosis := VSS.Strings.Conversions.To_Virtual_String (Message);
       end Report_Error;
 
-      U1 : VSS.Unicode.Code_Point;
-      U2 : VSS.Unicode.Code_Point;
-      U3 : VSS.Unicode.Code_Point;
-      U4 : VSS.Unicode.Code_Point;
+      Code  : VSS.Unicode.Code_Point;
+      Error : VSS.Implementation.UTF8_Encoding.UTF8_Decode_Error;
 
    begin
-      if Self.Current > Self.Buffer.Length then
-         Success := False;
-         Item := VSS.Characters.Virtual_Character'Val (0);
-
-         return;
-      end if;
-
       if Self.Incremental then
          if Self.Skip then
             Self.Skip := False;
@@ -90,152 +81,57 @@ package body Test_Text_Streams is
          end if;
       end if;
 
-      case Self.Buffer.Element (Self.Current) is
-         when 16#00# .. 16#7F# =>
-            --  1x code units sequence
-            --
-            --  00 .. 7F
+      VSS.Implementation.UTF8_Encoding.Decode
+        (Self.Buffer, Self.Current, Code, Success, Error);
 
-            Item :=
-              VSS.Characters.Virtual_Character'Val
-                (Self.Buffer.Element (Self.Current));
-            Self.Current := Self.Current + 1;
+      Item := VSS.Characters.Virtual_Character'Val (Code);
 
-         when 16#C2# .. 16#DF# =>
-            --  2x code units sequence
-            --
-            --  C2 .. DF | 80 .. BF
+      case Error is
+         when VSS.Implementation.UTF8_Encoding.None =>
+            null;
 
-            if Self.Current + 1 > Self.Buffer.Length then
-               raise Program_Error;
-            end if;
+         when VSS.Implementation.UTF8_Encoding.Incomplete_2 =>
+            Report_Error ("incomplete two code unit sequence");
 
-            U1 :=
-              VSS.Unicode.Code_Point (Self.Buffer.Element (Self.Current));
-            U2 :=
-              VSS.Unicode.Code_Point (Self.Buffer.Element (Self.Current + 1));
+         when VSS.Implementation.UTF8_Encoding.Incomplete_3 =>
+            Report_Error ("incomplete three code unit sequence");
 
-            if U2 not in 16#80# .. 16#BF# then
-               raise Program_Error;
-            end if;
+         when VSS.Implementation.UTF8_Encoding.Incomplete_4 =>
+            Report_Error ("incomplete four code unit sequence");
 
-            U1 := (U1 and 2#0001_1111#) * 2#0100_0000#;
-            U2 := U2 and 2#0011_1111#;
-
-            Item := VSS.Characters.Virtual_Character'Val (U1 or U2);
-            Self.Current := Self.Current + 2;
-
-         when 16#E0# .. 16#EF# =>
-            --  3x code units sequence
-            --
-            --  E0       | A0 .. BF | 80 .. BF
-            --  E1 .. EC | 80 .. BF | 80 .. BF
-            --  ED       | 80 .. 9F | 80 .. BF
-            --  EE .. EF | 80 .. BF | 80 .. BF
-
-            if Self.Current + 2 > Self.Buffer.Length then
-               Report_Error ("incomplete three code unit sequence");
-
-               return;
-            end if;
-
-            U1 :=
-              VSS.Unicode.Code_Point (Self.Buffer.Element (Self.Current));
-            U2 :=
-              VSS.Unicode.Code_Point (Self.Buffer.Element (Self.Current + 1));
-            U3 :=
-              VSS.Unicode.Code_Point (Self.Buffer.Element (Self.Current + 2));
-
-            if U1 = 16#E0# and U2 not in 16#A0# .. 16#BF# then
-               Report_Error
-                 ("invalid UTF-8 sequence (wrong second code unit of three"
-                     & " code units sequene)");
-
-               return;
-
-            elsif U1 in 16#E1# .. 16#EC# and U2 not in 16#80# .. 16#BF# then
-               Report_Error
-                 ("invalid UTF-8 sequence (wrong second code unit of three"
-                     & " code units sequene)");
-
-               return;
-
-            elsif U1 = 16#ED# and U2 not in 16#80# .. 16#9F# then
-               Report_Error
-                 ("invalid UTF-8 sequence (wrong second code unit of three"
-                     & " code units sequene)");
-
-               return;
-
-            elsif U1 in 16#EE# .. 16#EF# and U2 not in 16#80# .. 16#BF# then
-               raise Program_Error;
-            end if;
-
-            if U3 not in 16#80# .. 16#BF# then
-               raise Program_Error;
-            end if;
-
-            U1 := (U1 and 2#0000_1111#) * 2#01_0000_0000_0000#;
-            U2 := (U2 and 2#0011_1111#) * 2#0100_0000#;
-            U3 := U3 and 2#0011_1111#;
-
-            Item := VSS.Characters.Virtual_Character'Val (U1 or U2 or U3);
-            Self.Current := Self.Current + 3;
-
-         when 16#F0# .. 16#F4# =>
-            --  4x code units sequence
-            --
-            --  F0       | 90 .. BF | 80 .. BF | 80 .. BF
-            --  F1 .. F3 | 80 .. BF | 80 .. BF | 80 .. BF
-            --  F4       | 80 .. 8F | 80 .. BF | 80 .. BF
-
-            if Self.Current + 3 > Self.Buffer.Length then
-               raise Program_Error;
-            end if;
-
-            U1 :=
-              VSS.Unicode.Code_Point (Self.Buffer.Element (Self.Current));
-            U2 :=
-              VSS.Unicode.Code_Point (Self.Buffer.Element (Self.Current + 1));
-            U3 :=
-              VSS.Unicode.Code_Point (Self.Buffer.Element (Self.Current + 2));
-            U4 :=
-              VSS.Unicode.Code_Point (Self.Buffer.Element (Self.Current + 3));
-
-            if U1 = 16#F0# and U2 not in 16#90# .. 16#BF# then
-               raise Program_Error;
-
-            elsif U1 in 16#F1# .. 16#F3# and U2 not in 16#80# .. 16#BF# then
-               raise Program_Error;
-
-            elsif U1 = 16#F4# and U2 not in 16#80# .. 16#8F# then
-               Report_Error
-                 ("invalid UTF-8 sequence (wrong second code unit of four"
-                     & " code units sequene)");
-
-               return;
-            end if;
-
-            if U3 not in 16#80# .. 16#BF# then
-               raise Program_Error;
-            end if;
-
-            if U4 not in 16#80# .. 16#BF# then
-               raise Program_Error;
-            end if;
-
-            U1 := (U1 and 2#0000_0111#) * 2#0100_0000_0000_0000_0000#;
-            U2 := (U2 and 2#0011_1111#) * 2#010_000_0000_0000#;
-            U3 := (U3 and 2#0011_1111#) * 2#0100_0000#;
-            U4 := U4 and 2#0011_1111#;
-
-            Item :=
-              VSS.Characters.Virtual_Character'Val (U1 or U2 or U3 or U4);
-            Self.Current := Self.Current + 4;
-
-         when others =>
+         when VSS.Implementation.UTF8_Encoding.Invalid_1 =>
             Report_Error
               ("invalid UTF-8 sequence (wrong start code unit)");
+
+         when VSS.Implementation.UTF8_Encoding.Invalid_2_Of_2 =>
+            Report_Error
+              ("invalid UTF-8 sequence (wrong second code unit of"
+               & " two code units sequene)");
+
+         when VSS.Implementation.UTF8_Encoding.Invalid_2_Of_3 =>
+            Report_Error
+              ("invalid UTF-8 sequence (wrong second code unit of"
+               & " three code units sequene)");
+
+         when VSS.Implementation.UTF8_Encoding.Invalid_3_Of_3 =>
+            Report_Error
+              ("invalid UTF-8 sequence (wrong third code unit of"
+               & " three code units sequene)");
+
+         when VSS.Implementation.UTF8_Encoding.Invalid_2_Of_4 =>
+            Report_Error
+              ("invalid UTF-8 sequence (wrong second code unit of"
+               & " four code units sequene)");
+
+         when VSS.Implementation.UTF8_Encoding.Invalid_3_Of_4 =>
+            Report_Error
+              ("invalid UTF-8 sequence (wrong third code unit of"
+               & " four code units sequene)");
+
+         when VSS.Implementation.UTF8_Encoding.Invalid_4_Of_4 =>
+            Report_Error
+              ("invalid UTF-8 sequence (wrong forth code unit of"
+               & " four code units sequene)");
       end case;
    end Get;
 

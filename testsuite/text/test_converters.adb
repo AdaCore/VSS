5b0c9fa545651be0a9097a8eb2593ac41d3c6e6c
Vadim Godunko <godunko@adacore.com>
2021-03-14 11:53:05 +0300

    Improve tests for decoder to increase coverage.

--- testsuite/text/test_converters.adb
+++ testsuite/text/test_converters.adb
@@ -21,21 +21,22 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
+with Ada.Streams;
+
 with VSS.Stream_Element_Buffers.Conversions;
 with VSS.Strings.Converters.Decoders;
 
 procedure Test_Converters is
    use all type VSS.Strings.Converters.Converter_Flag;
 
-   D1 : VSS.Stream_Element_Buffers.Stream_Element_Buffer :=
-     VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-       ((16#EF#, 16#BB#, 16#BF#));
+   D1 : constant Ada.Streams.Stream_Element_Array :=
+     (16#EF#, 16#BB#, 16#BF#);
+   --  BOM only
 
-   D2 : VSS.Stream_Element_Buffers.Stream_Element_Buffer :=
-     VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-       ((16#C0#, 16#AF#, 16#E0#, 16#80#,  16#BF#, 16#F0#, 16#81#, 16#82#,
-         16#41#));
-   E2 : VSS.Strings.Virtual_String :=
+   D2 : constant Ada.Streams.Stream_Element_Array :=
+     (16#C0#, 16#AF#, 16#E0#, 16#80#,  16#BF#, 16#F0#, 16#81#, 16#82#,
+      16#41#);
+   E2 : constant VSS.Strings.Virtual_String :=
      VSS.Strings.To_Virtual_String
        ((Wide_Wide_Character'Val (16#FFFD#),
          Wide_Wide_Character'Val (16#FFFD#),
@@ -48,11 +49,10 @@ procedure Test_Converters is
          Wide_Wide_Character'Val (16#0041#)));
    --  Unicode 13.0, Table 3-8.
 
-   D3 : VSS.Stream_Element_Buffers.Stream_Element_Buffer :=
-     VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-       ((16#ED#, 16#A0#, 16#80#, 16#ED#,  16#BF#, 16#BF#, 16#ED#, 16#AF#,
-         16#41#));
-   E3 : VSS.Strings.Virtual_String :=
+   D3 : constant Ada.Streams.Stream_Element_Array :=
+     (16#ED#, 16#A0#, 16#80#, 16#ED#,  16#BF#, 16#BF#, 16#ED#, 16#AF#,
+      16#41#);
+   E3 : constant VSS.Strings.Virtual_String :=
      VSS.Strings.To_Virtual_String
        ((Wide_Wide_Character'Val (16#FFFD#),
          Wide_Wide_Character'Val (16#FFFD#),
@@ -65,11 +65,10 @@ procedure Test_Converters is
          Wide_Wide_Character'Val (16#0041#)));
    --  Unicode 13.0, Table 3-9.
 
-   D4 : VSS.Stream_Element_Buffers.Stream_Element_Buffer :=
-     VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-       ((16#F4#, 16#91#, 16#92#, 16#93#,  16#FF#, 16#41#, 16#80#, 16#BF#,
-         16#42#));
-   E4 : VSS.Strings.Virtual_String :=
+   D4 : constant Ada.Streams.Stream_Element_Array :=
+     (16#F4#, 16#91#, 16#92#, 16#93#,  16#FF#, 16#41#, 16#80#, 16#BF#,
+      16#42#);
+   E4 : constant VSS.Strings.Virtual_String :=
      VSS.Strings.To_Virtual_String
        ((Wide_Wide_Character'Val (16#FFFD#),
          Wide_Wide_Character'Val (16#FFFD#),
@@ -82,11 +81,10 @@ procedure Test_Converters is
          Wide_Wide_Character'Val (16#0042#)));
    --  Unicode 13.0, Table 3-10.
 
-   D5 : VSS.Stream_Element_Buffers.Stream_Element_Buffer :=
-     VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-       ((16#E1#, 16#80#, 16#E2#, 16#F0#,  16#91#, 16#92#, 16#F1#, 16#BF#,
-         16#41#));
-   E5 : VSS.Strings.Virtual_String :=
+   D5 : constant Ada.Streams.Stream_Element_Array :=
+     (16#E1#, 16#80#, 16#E2#, 16#F0#,  16#91#, 16#92#, 16#F1#, 16#BF#,
+      16#41#);
+   E5 : constant VSS.Strings.Virtual_String :=
      VSS.Strings.To_Virtual_String
        ((Wide_Wide_Character'Val (16#FFFD#),
          Wide_Wide_Character'Val (16#FFFD#),
@@ -95,19 +93,188 @@ procedure Test_Converters is
          Wide_Wide_Character'Val (16#0041#)));
    --  Unicode 13.0, Table 3-11.
 
-   D6 : constant VSS.Stream_Element_Buffers.Stream_Element_Buffer :=
-     VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-       ((16#41#, 16#D0#, 16#91#, 16#E0#,  16#A4#, 16#95#, 16#F0#, 16#90#,
-         16#8C#, 16#88#));
+   D6 : constant Ada.Streams.Stream_Element_Array :=
+     (16#41#, 16#D0#, 16#91#, 16#E0#,  16#A4#, 16#95#, 16#F0#, 16#90#,
+      16#8C#, 16#88#);
    E6 : constant VSS.Strings.Virtual_String := "AБक𐌈";
 
+   procedure Do_Test
+     (Encoded   : Ada.Streams.Stream_Element_Array;
+      Decoded   : VSS.Strings.Virtual_String;
+      Has_Error : Boolean);
+   --  Run decoder for UTF-8 encoding in two modes: block and incremental,
+   --  and check result.
+
+   -------------
+   -- Do_Test --
+   -------------
+
+   procedure Do_Test
+     (Encoded   : Ada.Streams.Stream_Element_Array;
+      Decoded   : VSS.Strings.Virtual_String;
+      Has_Error : Boolean) is
+   begin
+      --  Stream_Element_Array and block mode
+
+      declare
+         use type VSS.Strings.Virtual_String;
+
+         Decoder : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
+         Result  : VSS.Strings.Virtual_String;
+
+      begin
+         Decoder.Initialize ("utf-8");
+
+         if not Decoder.Is_Valid then
+            raise Program_Error;
+         end if;
+
+         Result := Decoder.Decode (Encoded);
+
+         if Result /= Decoded then
+            raise Program_Error;
+         end if;
+
+         if Decoder.Has_Error /= Has_Error then
+            raise Program_Error;
+         end if;
+
+         if Decoder.Error_Message.Is_Empty and Has_Error then
+            raise Program_Error;
+         end if;
+      end;
+
+      --  Stream_Element_Vector and block mode
+
+      declare
+         use type VSS.Strings.Virtual_String;
+
+         Decoder : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
+         Result  : VSS.Strings.Virtual_String;
+
+      begin
+         Decoder.Initialize ("utf-8");
+
+         if not Decoder.Is_Valid then
+            raise Program_Error;
+         end if;
+
+         Result :=
+           Decoder.Decode
+             (VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
+                (Encoded));
+
+         if Result /= Decoded then
+            raise Program_Error;
+         end if;
+
+         if Decoder.Has_Error /= Has_Error then
+            raise Program_Error;
+         end if;
+
+         if Decoder.Error_Message.Is_Empty and Has_Error then
+            raise Program_Error;
+         end if;
+      end;
+
+      --  Stream_Element_Array and incremental mode
+
+      declare
+         use type VSS.Strings.Virtual_String;
+
+         Decoder : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
+         Result  : VSS.Strings.Virtual_String;
+
+      begin
+         Decoder.Initialize ("utf-8");
+
+         if not Decoder.Is_Valid then
+            raise Program_Error;
+         end if;
+
+         for J in Encoded'Range loop
+            Result.Append
+              (Decoder.Decode
+                 (Ada.Streams.Stream_Element_Array'((1 => Encoded (J)))));
+         end loop;
+
+         if Result /= Decoded then
+            raise Program_Error;
+         end if;
+
+         if Decoder.Has_Error /= Has_Error then
+            raise Program_Error;
+         end if;
+
+         if Decoder.Error_Message.Is_Empty and Has_Error then
+            raise Program_Error;
+         end if;
+      end;
+
+      --  Stream_Element_Vector and incremental mode
+
+      declare
+         use type VSS.Strings.Virtual_String;
+
+         Decoder : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
+         Result  : VSS.Strings.Virtual_String;
+
+      begin
+         Decoder.Initialize ("utf-8");
+
+         if not Decoder.Is_Valid then
+            raise Program_Error;
+         end if;
+
+         for J in Encoded'Range loop
+            Result.Append
+              (Decoder.Decode
+                 (VSS.Stream_Element_Buffers.Conversions
+                    .Unchecked_From_Stream_Element_Array
+                      ((1 => Encoded (J)))));
+         end loop;
+
+         if Result /= Decoded then
+            raise Program_Error;
+         end if;
+
+         if Decoder.Has_Error /= Has_Error then
+            raise Program_Error;
+         end if;
+
+         if Decoder.Error_Message.Is_Empty and Has_Error then
+            raise Program_Error;
+         end if;
+      end;
+   end Do_Test;
+
+
 begin
+   --  Check invalid state of the decoder after object declaration without
+   --  initialization.
+
+   declare
+      Decoder : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
+
+   begin
+      if Decoder.Is_Valid then
+         raise Program_Error;
+      end if;
+   end;
+
+   --  Check processing of the BOM
+
    declare
       D : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
       S : VSS.Strings.Virtual_String;
 
    begin
       D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
+
+      if not D.Is_Valid then
+         raise Program_Error;
+      end if;
+
       S := D.Decode (D1);
 
       if not S.Is_Empty then
@@ -122,11 +289,13 @@ begin
    begin
       D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
 
-      for J in 1 .. D1.Length loop
+      if not D.Is_Valid then
+         raise Program_Error;
+      end if;
+
+      for J in D1'Range loop
          S.Append
-           (D.Decode
-              (VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-                 ((1 => D1 (J)))));
+           (D.Decode (Ada.Streams.Stream_Element_Array'((1 => D1 (J)))));
       end loop;
 
       if not S.Is_Empty then
@@ -134,183 +303,9 @@ begin
       end if;
    end;
 
-   declare
-      use type VSS.Strings.Virtual_String;
-
-      D : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
-      S : VSS.Strings.Virtual_String;
-
-   begin
-      D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
-      S := D.Decode (D2);
-
-      if S /= E2 then
-         raise Program_Error;
-      end if;
-   end;
-
-   declare
-      use type VSS.Strings.Virtual_String;
-
-      D : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
-      S : VSS.Strings.Virtual_String;
-
-   begin
-      D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
-
-      for J in 1 .. D2.Length loop
-         S.Append
-           (D.Decode
-              (VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-                 ((1 => D2 (J)))));
-      end loop;
-
-      if S /= E2 then
-         raise Program_Error;
-      end if;
-   end;
-
-   declare
-      use type VSS.Strings.Virtual_String;
-
-      D : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
-      S : VSS.Strings.Virtual_String;
-
-   begin
-      D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
-      S := D.Decode (D3);
-
-      if S /= E3 then
-         raise Program_Error;
-      end if;
-   end;
-
-   declare
-      use type VSS.Strings.Virtual_String;
-
-      D : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
-      S : VSS.Strings.Virtual_String;
-
-   begin
-      D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
-
-      for J in 1 .. D3.Length loop
-         S.Append
-           (D.Decode
-              (VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-                 ((1 => D3 (J)))));
-      end loop;
-
-      if S /= E3 then
-         raise Program_Error;
-      end if;
-   end;
-
-   declare
-      use type VSS.Strings.Virtual_String;
-
-      D : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
-      S : VSS.Strings.Virtual_String;
-
-   begin
-      D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
-      S := D.Decode (D4);
-
-      if S /= E4 then
-         raise Program_Error;
-      end if;
-   end;
-
-   declare
-      use type VSS.Strings.Virtual_String;
-
-      D : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
-      S : VSS.Strings.Virtual_String;
-
-   begin
-      D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
-
-      for J in 1 .. D4.Length loop
-         S.Append
-           (D.Decode
-              (VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-                 ((1 => D4 (J)))));
-      end loop;
-
-      if S /= E4 then
-         raise Program_Error;
-      end if;
-   end;
-
-   declare
-      use type VSS.Strings.Virtual_String;
-
-      D : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
-      S : VSS.Strings.Virtual_String;
-
-   begin
-      D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
-      S := D.Decode (D5);
-
-      if S /= E5 then
-         raise Program_Error;
-      end if;
-   end;
-
-   declare
-      use type VSS.Strings.Virtual_String;
-
-      D : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
-      S : VSS.Strings.Virtual_String;
-
-   begin
-      D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
-
-      for J in 1 .. D5.Length loop
-         S.Append
-           (D.Decode
-              (VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-                 ((1 => D5 (J)))));
-      end loop;
-
-      if S /= E5 then
-         raise Program_Error;
-      end if;
-   end;
-
-   declare
-      use type VSS.Strings.Virtual_String;
-
-      D : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
-      S : VSS.Strings.Virtual_String;
-
-   begin
-      D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
-      S := D.Decode (D6);
-
-      if S /= E6 then
-         raise Program_Error;
-      end if;
-   end;
-
-   declare
-      use type VSS.Strings.Virtual_String;
-
-      D : VSS.Strings.Converters.Decoders.Virtual_String_Decoder;
-      S : VSS.Strings.Virtual_String;
-
-   begin
-      D.Initialize ("utf-8", (Ignore_BOM => True, others => False));
-
-      for J in 1 .. D6.Length loop
-         S.Append
-           (D.Decode
-              (VSS.Stream_Element_Buffers.Conversions.Unchecked_From_Stream_Element_Array
-                 ((1 => D6 (J)))));
-      end loop;
-
-      if S /= E6 then
-         raise Program_Error;
-      end if;
-   end;
+   Do_Test (D2, E2, True);
+   Do_Test (D3, E3, True);
+   Do_Test (D4, E4, True);
+   Do_Test (D5, E5, True);
+   Do_Test (D6, E6, False);
 end Test_Converters;

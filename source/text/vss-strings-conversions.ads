12c8be9a9f4efb5da30f7c900459cb70df470cc2
Vadim Godunko <godunko@adacore.com>
2021-06-10 14:24:31 +0300

    U310-022 Subprograms to update content of Wide_Wide_String.

--- source/text/vss-strings-conversions.ads
+++ source/text/vss-strings-conversions.ads
@@ -1,5 +1,5 @@
 --
---  Copyright (C) 2020-2021, AdaCore
+--  Copyright (C) 2020-2022, AdaCore
 --
 --  SPDX-License-Identifier: Apache-2.0
 --
@@ -44,5 +44,57 @@ package VSS.Strings.Conversions is
      (Item : Virtual_String'Class)
       return Ada.Strings.Wide_Wide_Unbounded.Unbounded_Wide_Wide_String;
    --  Convert virtual string into Wide_Wide_String.
+
+   procedure Set_UTF_8_String
+     (Item : Virtual_String'Class;
+      Into : out Ada.Strings.UTF_Encoding.UTF_8_String);
+   --  Set given Into string to content of virtual string. Length of the Into
+   --  string must be equal to the length in code units of the content of the
+   --  virtual string encoded in UTF-8; otherwise Constraint_Error is raised.
+
+   procedure Set_UTF_8_String
+     (Item : Virtual_String'Class;
+      Last : out Natural;
+      Into : out Ada.Strings.UTF_Encoding.UTF_8_String);
+   --  Fill given Into string by content of virtual string. Data is written
+   --  starting from the first character. Remaining length of the string
+   --  must be at least the length in code units of the content of the
+   --  virtual string encoded in UTF-8; otherwise Constraint_Error is raised.
+
+   procedure Set_UTF_8_String
+     (Item : Virtual_String'Class;
+      From : Positive;
+      Last : out Natural;
+      Into : out Ada.Strings.UTF_Encoding.UTF_8_String);
+   --  Fill given Into string by content of virtual string. Data is written
+   --  starting From character. Remaining length of the string must be at
+   --  least the length in code units of the content of the virtual string
+   --  encoded in UTF-8; otherwise Constraint_Error is raised.
+
+   procedure Set_Wide_Wide_String
+     (Item : Virtual_String'Class;
+      Into : out Wide_Wide_String);
+   --  Set given Into string to content of virtual string. Length of the
+   --  string must be equal to the length in characters of the virtual
+   --  string; otherwise Constraint_Error is raised.
+
+   procedure Set_Wide_Wide_String
+     (Item : Virtual_String'Class;
+      Last : out Natural;
+      Into : out Wide_Wide_String);
+   --  Fill given Into string by content of virtual string. Data is written
+   --  starting from first character. Length of the string must be at
+   --  least the length in characters of the virtual string; otherwise
+   --  Constraint_Error is raised.
+
+   procedure Set_Wide_Wide_String
+     (Item : Virtual_String'Class;
+      From : Positive;
+      Last : out Natural;
+      Into : out Wide_Wide_String);
+   --  Fill given Into string by content of virtual string. Data is written
+   --  starting From character. Remaining length of the string must be
+   --  at least the length in characters of the virtual string; otherwise
+   --  Constraint_Error is raised.
 
 end VSS.Strings.Conversions;

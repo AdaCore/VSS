599fb8a487e116bfeb47971c7e133cbaec0f95d5
Vadim Godunko <godunko@adacore.com>
2021-07-05 12:59:00 +0300

    Reorder subprogram in alphabetical order.

--- source/text/implementation/vss-strings-conversions.adb
+++ source/text/implementation/vss-strings-conversions.adb
@@ -37,25 +37,35 @@ package body VSS.Strings.Conversions is
    --  must be equal to the length in characters of the virtual string;
    --  otherwise Constraint_Error is raised.
 
-   ---------------------
-   -- To_UTF_8_String --
-   ---------------------
+   -------------------------
+   -- Set_Wide_Wide_String --
+   -------------------------
 
-   function To_UTF_8_String
-     (Item : Virtual_String'Class)
-      return Ada.Strings.UTF_Encoding.UTF_8_String
+   procedure Set_Wide_Wide_String
+     (Item   : Virtual_String'Class;
+      String : out Wide_Wide_String)
    is
-      Handler : constant VSS.Implementation.Strings.String_Handler_Access :=
+      Handler  : constant VSS.Implementation.Strings.String_Handler_Access :=
         Item.Handler;
+      Position : VSS.Implementation.Strings.Cursor;
 
    begin
-      if Handler = null then
-         return "";
-
-      else
-         return Handler.To_UTF_8_String (Item.Data);
+      if Item.Character_Length /= String'Length then
+         raise Constraint_Error;
       end if;
-   end To_UTF_8_String;
+
+      if Item.Is_Empty then
+         return;
+      end if;
+
+      Handler.Before_First_Character (Item.Data, Position);
+
+      while Handler.Forward (Item.Data, Position) loop
+         String (String'First + Integer (Position.Index) - 1) :=
+           Wide_Wide_Character'Val
+             (Handler.Element (Item.Data, Position));
+      end loop;
+   end Set_Wide_Wide_String;
 
    -----------------------------------
    -- To_Unbounded_Wide_Wide_String --
@@ -86,6 +96,26 @@ package body VSS.Strings.Conversions is
          end if;
       end return;
    end To_Unbounded_Wide_Wide_String;
+
+   ---------------------
+   -- To_UTF_8_String --
+   ---------------------
+
+   function To_UTF_8_String
+     (Item : Virtual_String'Class)
+      return Ada.Strings.UTF_Encoding.UTF_8_String
+   is
+      Handler : constant VSS.Implementation.Strings.String_Handler_Access :=
+        Item.Handler;
+
+   begin
+      if Handler = null then
+         return "";
+
+      else
+         return Handler.To_UTF_8_String (Item.Data);
+      end if;
+   end To_UTF_8_String;
 
    -----------------------
    -- To_Virtual_String --
@@ -120,36 +150,6 @@ package body VSS.Strings.Conversions is
          end if;
       end return;
    end To_Virtual_String;
-
-   -------------------------
-   -- Set_Wide_Wide_String --
-   -------------------------
-
-   procedure Set_Wide_Wide_String
-     (Item   : Virtual_String'Class;
-      String : out Wide_Wide_String)
-   is
-      Handler  : constant VSS.Implementation.Strings.String_Handler_Access :=
-        Item.Handler;
-      Position : VSS.Implementation.Strings.Cursor;
-
-   begin
-      if Item.Character_Length /= String'Length then
-         raise Constraint_Error;
-      end if;
-
-      if Item.Is_Empty then
-         return;
-      end if;
-
-      Handler.Before_First_Character (Item.Data, Position);
-
-      while Handler.Forward (Item.Data, Position) loop
-         String (String'First + Integer (Position.Index) - 1) :=
-           Wide_Wide_Character'Val
-             (Handler.Element (Item.Data, Position));
-      end loop;
-   end Set_Wide_Wide_String;
 
    -------------------------
    -- To_Wide_Wide_String --

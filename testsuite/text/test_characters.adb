52bb61bc1cfbe0a8dd493586777a4d7789c545c5
Vadim Godunko <godunko@adacore.com>
2021-08-04 11:10:47 +0300

    Test value of General Category for all characters.

--- testsuite/text/test_characters.adb
+++ testsuite/text/test_characters.adb
@@ -26,6 +26,7 @@ with Ada.Strings.UTF_Encoding.Wide_Wide_Strings;
 
 with UCD.Characters;
 with UCD.Derived_Core_Properties_Loader;
+with UCD.Derived_General_Category_Loader;
 with UCD.Properties;
 with UCD.Property_Aliases_Loader;
 with UCD.Property_Value_Aliases_Loader;
@@ -64,6 +65,7 @@ procedure Test_Characters is
 
          UCD.Characters.Initialize_Character_Database;
 
+         UCD.Derived_General_Category_Loader.Load (UCD_Root);
          UCD.Derived_Core_Properties_Loader.Load (UCD_Root);
       end;
    end Initialize_UCD;
@@ -74,6 +76,78 @@ procedure Test_Characters is
 
    procedure Test_Properties is
       use type UCD.Properties.Property_Value_Access;
+
+      GC_Property : constant UCD.Properties.Property_Access :=
+        UCD.Properties.Resolve ("gc");
+      GC_Mapping  : constant
+        array (VSS.Characters.General_Category)
+          of UCD.Properties.Property_Value_Access :=
+          (VSS.Characters.Uppercase_Letter =>
+             UCD.Properties.Resolve (GC_Property, "Lu"),
+           VSS.Characters.Lowercase_Letter =>
+             UCD.Properties.Resolve (GC_Property, "Ll"),
+           VSS.Characters.Titlecase_Letter =>
+             UCD.Properties.Resolve (GC_Property, "Lt"),
+           VSS.Characters.Modifier_Letter =>
+             UCD.Properties.Resolve (GC_Property, "Lm"),
+           VSS.Characters.Other_Letter =>
+             UCD.Properties.Resolve (GC_Property, "Lo"),
+
+           VSS.Characters.Nonspacing_Mark =>
+             UCD.Properties.Resolve (GC_Property, "Mn"),
+           VSS.Characters.Spacing_Mark =>
+             UCD.Properties.Resolve (GC_Property, "Mc"),
+           VSS.Characters.Enclosing_Mark =>
+             UCD.Properties.Resolve (GC_Property, "Me"),
+
+           VSS.Characters.Decimal_Number =>
+             UCD.Properties.Resolve (GC_Property, "Nd"),
+           VSS.Characters.Letter_Number =>
+             UCD.Properties.Resolve (GC_Property, "Nl"),
+           VSS.Characters.Other_Number =>
+             UCD.Properties.Resolve (GC_Property, "No"),
+
+           VSS.Characters.Connector_Punctuation =>
+             UCD.Properties.Resolve (GC_Property, "Pc"),
+           VSS.Characters.Dash_Punctuation =>
+             UCD.Properties.Resolve (GC_Property, "Pd"),
+           VSS.Characters.Open_Punctuation =>
+             UCD.Properties.Resolve (GC_Property, "Ps"),
+           VSS.Characters.Close_Punctuation =>
+             UCD.Properties.Resolve (GC_Property, "Pe"),
+           VSS.Characters.Initial_Punctuation =>
+             UCD.Properties.Resolve (GC_Property, "Pi"),
+           VSS.Characters.Final_Punctuation =>
+             UCD.Properties.Resolve (GC_Property, "Pf"),
+           VSS.Characters.Other_Punctuation =>
+             UCD.Properties.Resolve (GC_Property, "Po"),
+
+           VSS.Characters.Math_Symbol =>
+             UCD.Properties.Resolve (GC_Property, "Sm"),
+           VSS.Characters.Currency_Symbol =>
+             UCD.Properties.Resolve (GC_Property, "Sc"),
+           VSS.Characters.Modifier_Symbol =>
+             UCD.Properties.Resolve (GC_Property, "Sk"),
+           VSS.Characters.Other_Symbol =>
+             UCD.Properties.Resolve (GC_Property, "So"),
+
+           VSS.Characters.Space_Separator =>
+             UCD.Properties.Resolve (GC_Property, "Zs"),
+           VSS.Characters.Line_Separator =>
+             UCD.Properties.Resolve (GC_Property, "Zl"),
+           VSS.Characters.Paragraph_Separator =>
+             UCD.Properties.Resolve (GC_Property, "Zp"),
+
+           VSS.Characters.Control =>
+             UCD.Properties.Resolve (GC_Property, "Cc"),
+           VSS.Characters.Format =>
+             UCD.Properties.Resolve (GC_Property, "Cf"),
+           VSS.Characters.Surrogate =>
+             UCD.Properties.Resolve (GC_Property, "Cs"),
+           VSS.Characters.Private_Use =>
+             UCD.Properties.Resolve (GC_Property, "Co"),
+           VSS.Characters.Unassigned =>
+             UCD.Properties.Resolve (GC_Property, "Cn"));
 
       Lowercase_Property : constant UCD.Properties.Property_Access :=
         UCD.Properties.Resolve ("Lowercase");
@@ -96,6 +170,15 @@ procedure Test_Characters is
 
    begin
       for Character in VSS.Characters.Virtual_Character'Range loop
+         --  General Category
+
+         Test_Support.Assert
+           (UCD.Characters.Get
+              (VSS.Characters.Virtual_Character'Pos (Character), GC_Property)
+            = GC_Mapping (VSS.Characters.Get_General_Category (Character)));
+
+         --  Lowercase
+
          if VSS.Characters.Get_Lowercase (Character) then
             Test_Support.Assert
               (UCD.Characters.Get
@@ -109,6 +192,8 @@ procedure Test_Characters is
                   Lowercase_Property) = Lowercase_N);
          end if;
 
+         --  Uppercase
+
          if VSS.Characters.Get_Uppercase (Character) then
             Test_Support.Assert
               (UCD.Characters.Get
@@ -121,6 +206,8 @@ procedure Test_Characters is
                  (VSS.Characters.Virtual_Character'Pos (Character),
                   Uppercase_Property) = Uppercase_N);
          end if;
+
+         --  Cased
 
          if VSS.Characters.Get_Cased (Character) then
             Test_Support.Assert
---
+++ tools/ucd/ucd-derived_general_category_loader.adb
@@ -0,0 +1,72 @@
+------------------------------------------------------------------------------
+--                        M A G I C   R U N T I M E                         --
+--                                                                          --
+--                       Copyright (C) 2021, AdaCore                        --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Strings.Wide_Wide_Unbounded; use Ada.Strings.Wide_Wide_Unbounded;
+
+with UCD.Characters;
+with UCD.Data_File_Loaders;
+with UCD.Properties;
+
+package body UCD.Derived_General_Category_Loader is
+
+   ----------
+   -- Load --
+   ----------
+
+   procedure Load (UCD_Root : Wide_Wide_String) is
+      GC_Property : constant not null UCD.Properties.Property_Access :=
+        UCD.Properties.Resolve ("gc");
+
+      Value_Field : constant Data_File_Loaders.Field_Index := 1;
+      --  Index of the data field with the value of the property.
+
+      Loader : UCD.Data_File_Loaders.File_Loader;
+
+   begin
+      Loader.Open (UCD_Root, "extracted/DerivedGeneralCategory.txt");
+
+      while not Loader.End_Of_File loop
+         declare
+            First_Code : UCD.Code_Point;
+            Last_Code  : UCD.Code_Point;
+
+         begin
+            Loader.Get_Code_Point_Range (First_Code, Last_Code);
+
+            declare
+               Value : constant not null Properties.Property_Value_Access :=
+                 UCD.Properties.Resolve
+                   (GC_Property, Loader.Get_Field (Value_Field));
+
+            begin
+               for Code in First_Code .. Last_Code loop
+                  UCD.Characters.Set (Code, GC_Property, Value);
+               end loop;
+            end;
+
+            Loader.Skip_Line;
+         end;
+      end loop;
+   end Load;
+
+end UCD.Derived_General_Category_Loader;

ecc830f96f7783e39471aaea7d621e7adaddda91
Vadim Godunko <godunko@adacore.com>
2021-08-05 20:45:47 +0300

    Load CaseFolding.txt file.

---
+++ tools/ucd/ucd-case_folding_loader.adb
@@ -0,0 +1,112 @@
+------------------------------------------------------------------------------
+--                        M A G I C   R U N T I M E                         --
+--                                                                          --
+--                       Copyright (C) 2021, AdaCore                        --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with UCD.Characters;
+with UCD.Data_File_Loaders;
+with UCD.Properties;
+
+package body UCD.Case_Folding_Loader is
+
+   ----------
+   -- Load --
+   ----------
+
+   procedure Load (UCD_Root : Wide_Wide_String) is
+      --  Case_Folding, Simple_Case_Folding properties.
+
+      SCF_Property : constant not null Properties.Property_Access :=
+        Properties.Resolve ("scf");
+      CF_Property  : constant not null Properties.Property_Access :=
+        Properties.Resolve ("cf");
+
+      --  <status> and <mapping> fields.
+
+      Status_Field  : constant Data_File_Loaders.Field_Index := 1;
+      Mapping_Field : constant Data_File_Loaders.Field_Index := 2;
+
+      Loader : UCD.Data_File_Loaders.File_Loader;
+
+   begin
+      --  Mark properties as string properties, this can't be determined
+      --  during initial database construction.
+
+      SCF_Property.Is_String := True;
+      CF_Property.Is_String  := True;
+
+      Loader.Open (UCD_Root, "CaseFolding.txt");
+
+      while not Loader.End_Of_File loop
+         declare
+            First_Code : UCD.Code_Point;
+            Last_Code  : UCD.Code_Point;
+            Code       : UCD.Code_Point;
+
+         begin
+            Loader.Get_Code_Point_Range (First_Code, Last_Code);
+
+            if First_Code /= Last_Code then
+               raise Program_Error;
+            end if;
+
+            Code := First_Code;
+
+            declare
+               Mapping_Data  : constant UCD.Code_Point_Vectors.Vector :=
+                 Loader.Get_Field (Mapping_Field);
+               Mapping_Value :
+                 constant not null Properties.Property_Value_Access :=
+                   new Properties.Property_Value'
+                    (Names                           => <>,
+                     Is_Used                         => <>,
+                     Canonical_Combining_Class_Value => <>,
+                     String                          => Mapping_Data);
+               Status_Value  : constant Wide_Wide_String :=
+                 Loader.Get_Field (Status_Field);
+
+            begin
+               if Status_Value = "C" then
+                  Characters.Set (Code, SCF_Property, Mapping_Value);
+                  Characters.Set (Code, CF_Property, Mapping_Value);
+
+               elsif Status_Value = "F" then
+                  Characters.Set (Code, CF_Property, Mapping_Value);
+
+               elsif Status_Value = "S" then
+                  Characters.Set (Code, SCF_Property, Mapping_Value);
+
+               elsif Status_Value = "T" then
+                  --  Case folding mapping for Turkic language is ignored.
+
+                  null;
+
+               else
+                  raise Program_Error;
+               end if;
+            end;
+
+            Loader.Skip_Line;
+         end;
+      end loop;
+   end Load;
+
+end UCD.Case_Folding_Loader;

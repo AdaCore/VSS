d0fe3975af18bbee9ab55c8f3cdcd12de0b49931
Vadim Godunko <godunko@adacore.com>
2021-08-20 15:14:20 +0300

    Conversion subprogram to convert from UTF-8 data from Unbounded_String.

--- source/text/vss-strings-conversions.ads
+++ source/text/vss-strings-conversions.ads
@@ -22,6 +22,7 @@
 ------------------------------------------------------------------------------
 --  Conversion between standard string types and Magic_String.
 
+with Ada.Strings.Unbounded;
 with Ada.Strings.UTF_Encoding;
 with Ada.Strings.Wide_Wide_Unbounded;
 
@@ -29,6 +30,10 @@ package VSS.Strings.Conversions is
 
    function To_Virtual_String
      (Item : Ada.Strings.UTF_Encoding.UTF_8_String) return Virtual_String;
+
+   function To_Virtual_String
+     (Item : Ada.Strings.Unbounded.Unbounded_String) return Virtual_String;
+   --  Convert UTF-8 encoded unbounded string into Virtual_String.
 
    function To_UTF_8_String
      (Item : Virtual_String'Class)

3a14b2b43c252e620a069a0c8e510904bf95caba
Vadim Godunko <godunko@adacore.com>
2021-09-14 22:43:39 +0300

    Generate CCC of the last code point in full decomposition.

--- tools/gen_ucd/gen_ucd.ads
+++ tools/gen_ucd/gen_ucd.ads
@@ -43,8 +43,11 @@ package Gen_UCD is
    type Unsigned_14 is mod 2 ** 14 with Size => 14;
 
    type Unsigned_16 is mod 2 ** 16 with Size => 16;
+   type Unsigned_17 is mod 2 ** 17 with Size => 17;
 
    type Unsigned_32 is mod 2 ** 32 with Size => 32;
+
+   type Unsigned_64 is mod 2 ** 64 with Size => 64;
 
    --------------------------
    -- UTF-8 encoding types --

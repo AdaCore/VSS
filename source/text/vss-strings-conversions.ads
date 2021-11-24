66914e9ea6b1f20c695939832231c284659155ea
Vadim Godunko <godunko@adacore.com>
2021-11-24 09:45:41 +0300

    Conversion from Unbounded_Wide_Wide_String to Virtual_String.

--- source/text/vss-strings-conversions.ads
+++ source/text/vss-strings-conversions.ads
@@ -37,6 +37,11 @@ package VSS.Strings.Conversions is
      (Item : Ada.Strings.Unbounded.Unbounded_String) return Virtual_String;
    --  Convert UTF-8 encoded unbounded string into Virtual_String.
 
+   function To_Virtual_String
+     (Item : Ada.Strings.Wide_Wide_Unbounded.Unbounded_Wide_Wide_String)
+      return Virtual_String;
+   --  Convert UCS-4 encoded unbounded string into Virtual_String.
+
    function To_UTF_8_String
      (Item : Virtual_String'Class)
       return Ada.Strings.UTF_Encoding.UTF_8_String;

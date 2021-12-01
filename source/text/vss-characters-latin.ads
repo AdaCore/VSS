be327b50fc84ccac31fe90c49b4f365d754dd90a
Vadim Godunko <godunko@adacore.com>
2021-12-01 23:41:11 +0300

    Package for constants for named characters.

---
+++ source/text/vss-characters-latin.ads
@@ -0,0 +1,35 @@
+------------------------------------------------------------------------------
+--                        M A G I C   R U N T I M E                         --
+--                                                                          --
+--                       Copyright (C) 2021, AdaCore                        --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+--  Constants for named characters.
+
+package VSS.Characters.Latin is
+
+   pragma Preelaborate;
+
+   Nul       : constant Virtual_Character := Virtual_Character'Val (16#0000#);
+
+   Backspace : constant Virtual_Character := Virtual_Character'Val (16#0008#);
+
+   Line_Feed : constant Virtual_Character := Virtual_Character'Val (16#000A#);
+
+end VSS.Characters.Latin;

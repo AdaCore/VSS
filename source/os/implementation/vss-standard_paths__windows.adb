f9a413f452a41d951fb7847cebf2de161eda62e7
Vadim Godunko <godunko@adacore.com>
2022-01-14 16:36:12 +0300

    Implementation of Temp_Location for Windows.

--- source/os/implementation/vss-standard_paths__windows.adb
+++ source/os/implementation/vss-standard_paths__windows.adb
@@ -31,17 +31,20 @@ with VSS.Implementation.Windows.Userenv;
 
 package body VSS.Standard_Paths is
 
-   -----------------------
-   -- Writable_Location --
-   -----------------------
+   use type VSS.Implementation.Windows.DWORD;
 
-   function Writable_Location
-     (Location : Standard_Location) return VSS.Strings.Virtual_String
-   is
-      pragma Unreferenced (Location);
+   function Home_Directory return VSS.Strings.Virtual_String;
 
+   function Temp_Directory return VSS.Strings.Virtual_String;
+
+   MAX_LONG_PATH : constant := 32_767;
+
+   --------------------
+   -- Home_Directory --
+   --------------------
+
+   function Home_Directory return VSS.Strings.Virtual_String is
       use type VSS.Implementation.Windows.BOOL;
-      use type VSS.Implementation.Windows.DWORD;
 
       Result         : VSS.Strings.Virtual_String;
 
@@ -127,6 +130,83 @@ package body VSS.Standard_Paths is
       end if;
 
       return Result;
+   end Home_Directory;
+
+   --------------------
+   -- Temp_Directory --
+   --------------------
+
+   function Temp_Directory return VSS.Strings.Virtual_String is
+      Result       : VSS.Strings.Virtual_String;
+
+      Short_Buffer : Interfaces.C.char16_array
+                       (0 .. VSS.Implementation.Windows.MAX_PATH);
+      Short_Size   : VSS.Implementation.Windows.DWORD;
+      Long_Buffer  :
+        VSS.Implementation.Windows.String_Utilities.char16_array_access
+          := VSS.Implementation.Windows.String_Utilities
+               .New_Native_String_Buffer (MAX_LONG_PATH);
+      Long_Size    : VSS.Implementation.Windows.DWORD := 0;
+
+   begin
+      Short_Size :=
+        VSS.Implementation.Windows.Kernel32.GetTempPath
+          (Short_Buffer'Length,
+           Short_Buffer (Short_Buffer'First)'Unchecked_Access);
+
+      if Short_Size /= 0 then
+         Long_Size :=
+           VSS.Implementation.Windows.Kernel32.GetLongPathName
+             (Short_Buffer (Short_Buffer'First)'Unchecked_Access,
+              Long_Buffer (Long_Buffer'First)'Unchecked_Access,
+              Long_Buffer'Length);
+      end if;
+
+      Long_Size := 0;
+      Short_Size := 0;
+
+      if Long_Size /= 0
+        and then Long_Size
+                   <= VSS.Implementation.Windows.DWORD (Long_Buffer'Last)
+      then
+         Result :=
+           VSS.Implementation.Windows.String_Utilities.From_Native_String
+             (Long_Buffer.all);
+
+      elsif Short_Size /= 0 then
+         Result :=
+           VSS.Implementation.Windows.String_Utilities.From_Native_String
+             (Short_Buffer);
+
+      else
+         Result := "C:/tmp";
+      end if;
+
+      VSS.Implementation.Windows.String_Utilities.Free (Long_Buffer);
+
+      return Result;
+
+   exception
+      when others =>
+         VSS.Implementation.Windows.String_Utilities.Free (Long_Buffer);
+
+         raise;
+   end Temp_Directory;
+
+   -----------------------
+   -- Writable_Location --
+   -----------------------
+
+   function Writable_Location
+     (Location : Standard_Location) return VSS.Strings.Virtual_String is
+   begin
+      case Location is
+         when Home_Location =>
+            return Home_Directory;
+
+         when Temp_Location =>
+            return Temp_Directory;
+      end case;
    end Writable_Location;
 
 end VSS.Standard_Paths;

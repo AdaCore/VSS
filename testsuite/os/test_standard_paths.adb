f58b4c7013ab9b9aa10630d987fc6d9267b74626
Vadim Godunko <godunko@adacore.com>
2022-01-14 16:36:43 +0300

    Test of Temp_Location.

--- testsuite/os/test_standard_paths.adb
+++ testsuite/os/test_standard_paths.adb
@@ -32,10 +32,16 @@ procedure Test_Standard_Paths is
    Home : constant VSS.Strings.Virtual_String :=
      VSS.Standard_Paths.Writable_Location
        (VSS.Standard_Paths.Home_Location);
+   Temp : constant VSS.Strings.Virtual_String :=
+     VSS.Standard_Paths.Writable_Location
+       (VSS.Standard_Paths.Temp_Location);
 
 begin
    Ada.Wide_Wide_Text_IO.Put_Line
      (VSS.Strings.Conversions.To_Wide_Wide_String (Home));
+   Ada.Wide_Wide_Text_IO.Put_Line
+     (VSS.Strings.Conversions.To_Wide_Wide_String (Temp));
 
    Test_Support.Assert (not Home.Is_Empty);
+   Test_Support.Assert (not Temp.Is_Empty);
 end Test_Standard_Paths;

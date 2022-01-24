0414e5a33998d9b6f63381e1ff13cc03408acdb2
Vadim Godunko <godunko@adacore.com>
2022-01-24 10:55:10 +0300

    Process_Environment.Value_Paths subprogram.

--- source/os/implementation/vss-environments-platform__posix.adb
+++ source/os/implementation/vss-environments-platform__posix.adb
@@ -21,43 +21,16 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
-with VSS.String_Vectors;
-with VSS.Strings;
+separate (VSS.Environments)
+package body Platform is
 
-package VSS.Environments is
+   ---------------------------
+   -- Native_Path_Separator --
+   ---------------------------
 
-   type Process_Environment is tagged private;
+   function Native_Path_Separator return VSS.Characters.Virtual_Character is
+   begin
+      return ':';
+   end Native_Path_Separator;
 
-   function Contains
-     (Self : Process_Environment'Class;
-      Name : VSS.Strings.Virtual_String) return Boolean;
-   --  Return True when environment variable with the given name is set in
-   --  the environment.
-
-   function Value
-     (Self    : Process_Environment'Class;
-      Name    : VSS.Strings.Virtual_String;
-      Default : VSS.Strings.Virtual_String := VSS.Strings.Empty_Virtual_String)
-      return VSS.Strings.Virtual_String;
-   --  Return value of the environment variable with the given name or given
-   --  default value otherwise.
-
-   function Value_Paths
-     (Self             : Process_Environment'Class;
-      Name             : VSS.Strings.Virtual_String;
-      Keep_Empty_Paths : Boolean                                  := True;
-      Default          : VSS.String_Vectors.Virtual_String_Vector :=
-        VSS.String_Vectors.Empty_Virtual_String_Vector)
-      return VSS.String_Vectors.Virtual_String_Vector;
-   --  Return value of the environment variable with the given name if
-   --  defined, otherwise return given default value. Value of the environment
-   --  variable is split into the vector with platform specific separator
-   --  (':' on POSIX and ';' on Windows).
-
-private
-
-   type Process_Environment is tagged record
-      null;
-   end record;
-
-end VSS.Environments;
+end Platform;
--- source/os/implementation/vss-environments.adb
+++ source/os/implementation/vss-environments.adb
@@ -23,9 +23,19 @@
 
 --  XXX Right now only simple wrappers are implemented.
 
+with VSS.Characters;
 with VSS.Implementation.Environment_Utilities;
 
 package body VSS.Environments is
+
+   package Platform is
+
+      function Native_Path_Separator return VSS.Characters.Virtual_Character;
+      --  Return path separator of underlying platform.
+
+   end Platform;
+
+   package body Platform is separate;
 
    --------------
    -- Contains --
@@ -59,5 +69,31 @@ package body VSS.Environments is
    begin
       return VSS.Implementation.Environment_Utilities.Get_Env (Name, Default);
    end Value;
+
+   -----------------
+   -- Value_Paths --
+   -----------------
+
+   function Value_Paths
+     (Self             : Process_Environment'Class;
+      Name             : VSS.Strings.Virtual_String;
+      Keep_Empty_Paths : Boolean                                  := True;
+      Default          : VSS.String_Vectors.Virtual_String_Vector :=
+        VSS.String_Vectors.Empty_Virtual_String_Vector)
+      return VSS.String_Vectors.Virtual_String_Vector
+   is
+      pragma Unreferenced (Self);
+
+      Value : constant VSS.Strings.Virtual_String :=
+        VSS.Implementation.Environment_Utilities.Get_Env (Name);
+
+   begin
+      if Value.Is_Null then
+         return Default;
+
+      else
+         return Value.Split (Platform.Native_Path_Separator, Keep_Empty_Paths);
+      end if;
+   end Value_Paths;
 
 end VSS.Environments;

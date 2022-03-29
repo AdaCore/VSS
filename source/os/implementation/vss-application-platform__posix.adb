a6d1c0916473b43cda18b0aa2ccdb300bf059891
Vadim Godunko <godunko@adacore.com>
2022-03-29 12:34:18 +0300

    V324-027 Implementation of Application.Arguments on POSIX.

--- source/os/implementation/vss-application-platform__posix.adb
+++ source/os/implementation/vss-application-platform__posix.adb
@@ -20,34 +20,52 @@
 -- <http://www.gnu.org/licenses/>.                                          --
 --                                                                          --
 ------------------------------------------------------------------------------
+--  This is GNAT specific package, it use symbols declared in argv.c file
+--  of the GNAT Run Time Library.
+--
+--  Note: only UTF-8 locales are supported.
 
-package body VSS.Application is
+with Interfaces.C.Pointers;
+with Interfaces.C.Strings;
 
-   package Platform is
+with VSS.Strings.Conversions;
 
-      function Arguments return VSS.String_Vectors.Virtual_String_Vector;
-      --  Returns arguments provided in command line, except executable name.
+separate (VSS.Application)
+package body Platform is
 
-   end Platform;
+   type chars_ptr_Array is
+     array (Natural range <>) of aliased Interfaces.C.Strings.chars_ptr;
 
-   package body Platform is separate;
+   package chars_ptr_Pointers is
+     new Interfaces.C.Pointers
+       (Natural,
+        Interfaces.C.Strings.chars_ptr,
+        chars_ptr_Array,
+        Interfaces.C.Strings.Null_Ptr);
+
+   GNAT_Argc : constant Interfaces.C.int
+     with Import, Convention => C, Link_Name => "gnat_argc";
+   GNAT_Argv : constant chars_ptr_Pointers.Pointer
+     with Import, Convention => C, Link_Name => "gnat_argv";
 
    ---------------
    -- Arguments --
    ---------------
 
    function Arguments return VSS.String_Vectors.Virtual_String_Vector is
+      Args : constant chars_ptr_Array :=
+        chars_ptr_Pointers.Value
+          (GNAT_Argv, Interfaces.C.ptrdiff_t (GNAT_Argc));
+
    begin
-      return Platform.Arguments;
+      return Result : VSS.String_Vectors.Virtual_String_Vector do
+         for Index in 1 .. Args'Last loop
+            Result.Append
+              (VSS.Strings.Conversions.To_Virtual_String
+                 (Interfaces.C.Strings.Value (Args (Index))));
+            --  XXX Locale specific converter should be used here.
+         end loop;
+      end return;
    end Arguments;
 
-   ------------------------
-   -- System_Environment --
-   ------------------------
-
-   function System_Environment return VSS.Environments.Process_Environment is
-   begin
-      return Result : VSS.Environments.Process_Environment;
-   end System_Environment;
-
-end VSS.Application;
+end Platform;

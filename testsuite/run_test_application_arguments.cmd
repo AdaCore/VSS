1be698316355ae9dbc4b530df5a0cbfc6f091a67
Vadim Godunko <godunko@adacore.com>
2022-03-29 17:07:51 +0300

    V324-027 Use .cmd file to run test on Windows

---
+++ testsuite/run_test_application_arguments.cmd
@@ -0,0 +1,4 @@
+::  Run test driver executable and pass necessary arguments
+::  It is workaround of failure to process command line arguments with Unicode
+::  characters by Makefile/bash on GitHub Actions.
+.objs\tests\test_application_arguments.exe hello привет გამარჯობა 👋

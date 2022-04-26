8aa46cebab2593e392f55d2591f03ec9df498022
Maxim Reznik <reznik@adacore.com>
2022-04-26 17:24:14 +0300

    V426-005 Add a test case

--- testsuite/regexp/test_regexp-test_v426_005.adb
+++ testsuite/regexp/test_regexp-test_v426_005.adb
@@ -21,17 +21,32 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
-with Test_Support;
+with VSS.Regular_Expressions;
+with VSS.Strings;
 
-procedure Test_Regexp is
-   pragma Style_Checks ("gnaty-s");
+separate (Test_Regexp)
+procedure Test_V426_005 is
 
-   procedure Test_V406_014 is separate;
-   procedure Test_V406_018 is separate;
-   procedure Test_V426_005 is separate;
+   use type VSS.Strings.Virtual_String;
 
+   --  Check a negated character classes regexp.
+
+   R1 : constant VSS.Regular_Expressions.Regular_Expression :=
+     VSS.Regular_Expressions.To_Regular_Expression ("a[^\p{digit}a-fx]b");
+   M : VSS.Regular_Expressions.Regular_Expression_Match;
+
+   A1B : constant VSS.Strings.Virtual_String := "a1b";
+   ACB : constant VSS.Strings.Virtual_String := "acb";
+   AXB : constant VSS.Strings.Virtual_String := "axb";
+   AGB : constant VSS.Strings.Virtual_String := "aGb";
 begin
-   Test_V406_014;
-   Test_V406_018;
-   Test_V426_005;
-end Test_Regexp;
+   M := R1.Match (A1B);
+   Test_Support.Assert (not M.Has_Match);
+   M := R1.Match (ACB);
+   Test_Support.Assert (not M.Has_Match);
+   M := R1.Match (AXB);
+   Test_Support.Assert (not M.Has_Match);
+   M := R1.Match (AGB);
+   Test_Support.Assert (M.Has_Match and then M.Captured = "aGb");
+
+end Test_V426_005;

5b5949b9cc7e40fc9ef74789cb5ceed036546bc2
Vadim Godunko <godunko@adacore.com>
2022-05-19 16:30:40 +0300

    V509-015 Fix errors detected by newer compiler.

--- source/text/implementation/vss-strings-cursors-internals.adb
+++ source/text/implementation/vss-strings-cursors-internals.adb
@@ -59,20 +59,20 @@ package body VSS.Strings.Cursors.Internals is
      (Self  : VSS.Strings.Cursors.Abstract_Cursor'Class;
       Owner : VSS.Strings.Virtual_String'Class) return Boolean
    is
-      use type VSS.Implementation.Referrers.Magic_String_Access;
+      use type VSS.Implementation.Referrers.Virtual_String_Access;
 
    begin
       if Self in VSS.Implementation.Referrers.Referal_Base'Class then
          return
-           VSS.Implementation.Referrers.Referal_Base (Self).Owner
+           VSS.Implementation.Referrers.Referal_Base'Class (Self).Get_Owner
              = Owner'Unrestricted_Access;
 
       elsif Self
               in VSS.Implementation.Referrers.Referal_Limited_Base'Class
       then
          return
-           VSS.Implementation.Referrers.Referal_Limited_Base (Self).Owner
-             = Owner'Unrestricted_Access;
+           VSS.Implementation.Referrers.Referal_Limited_Base'Class
+             (Self).Get_Owner = Owner'Unrestricted_Access;
 
       else
          raise Program_Error;

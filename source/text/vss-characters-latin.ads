79c167141d9d5cd932f170df18744f106295c8c8
Vadim Godunko <godunko@adacore.com>
2022-06-03 13:32:02 +0300

    More Latin characters

--- source/text/vss-characters-latin.ads
+++ source/text/vss-characters-latin.ads
@@ -13,7 +13,32 @@ package VSS.Characters.Latin is
    Nul       : constant Virtual_Character := Virtual_Character'Val (16#0000#);
 
    Backspace : constant Virtual_Character := Virtual_Character'Val (16#0008#);
-
+   Character_Tabulation : constant Virtual_Character :=
+     Virtual_Character'Val (16#0009#);
    Line_Feed : constant Virtual_Character := Virtual_Character'Val (16#000A#);
+
+   Form_Feed : constant Virtual_Character := Virtual_Character'Val (16#000C#);
+   Carriage_Return : constant Virtual_Character :=
+     Virtual_Character'Val (16#000D#);
+
+   Space     : constant Virtual_Character := Virtual_Character'Val (16#0020#);
+
+   Quotation_Mark    : constant Virtual_Character :=
+     Virtual_Character'Val (16#0022#);
+
+   Ampersand         : constant Virtual_Character :=
+     Virtual_Character'Val (16#0026#);
+   Apostrophe        : constant Virtual_Character :=
+     Virtual_Character'Val (16#0027#);
+
+   Less_Than_Sign    : constant Virtual_Character :=
+     Virtual_Character'Val (16#003C#);
+   Equals_Sign       : constant Virtual_Character :=
+     Virtual_Character'Val (16#003D#);
+   Greater_Than_Sign : constant Virtual_Character :=
+     Virtual_Character'Val (16#003E#);
+
+   Grave_Accent      : constant Virtual_Character :=
+     Virtual_Character'Val (16#0060#);
 
 end VSS.Characters.Latin;

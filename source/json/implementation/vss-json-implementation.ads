eabf30927a968551eb73cd719b2b7de3867f0bd9
Vadim Godunko <godunko@adacore.com>
2022-09-07 13:45:34 +0300

    Disable documentation generation for implementation packages.

--- source/json/implementation/vss-json-implementation.ads
+++ source/json/implementation/vss-json-implementation.ads
@@ -4,6 +4,12 @@
 --  SPDX-License-Identifier: Apache-2.0
 --
 
+--  @private
+--
+--  Root of hierarchy of the internal implementation units. All code in this
+--  hierarchy is subject to change. Applications MUST NOT use this package and
+--  its children packages.
+
 package VSS.JSON.Implementation is
 
    pragma Preelaborate;

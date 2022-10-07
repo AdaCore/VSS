911b92a6b1e0d37c54c9a3b6d9978c44641c102e
Vadim Godunko <godunko@adacore.com>
2022-10-07 17:14:41 +0400

    V805-018 Decoder of ISO-8859-8

---
+++ source/text/implementation/vss-strings-converters-decoders-iso88598.adb
@@ -0,0 +1,147 @@
+--
+--  Copyright (C) 2022, AdaCore
+--
+--  SPDX-License-Identifier: Apache-2.0
+--
+
+with VSS.Implementation.String_Configuration;
+
+package body VSS.Strings.Converters.Decoders.ISO88598 is
+
+   ------------
+   -- Decode --
+   ------------
+
+   overriding procedure Decode
+     (Self   : in out ISO88598_Decoder;
+      Source : Ada.Streams.Stream_Element_Array;
+      Target : out VSS.Implementation.Strings.String_Data)
+   is
+      use type Ada.Streams.Stream_Element_Offset;
+      use type VSS.Unicode.Code_Point;
+
+      Index  : Ada.Streams.Stream_Element_Offset := Source'First;
+      Byte   : Ada.Streams.Stream_Element;
+      Offset : VSS.Implementation.Strings.Cursor_Offset := (0, 0, 0);
+
+   begin
+      if (Self.Error and Self.Flags (Stop_On_Error))
+        or else Source'Last < Source'First
+      then
+         --  Error was encountered in "stop on error" mode or source data is
+         --  empty: return "null" string.
+
+         Target := VSS.Implementation.Strings.Null_String_Data;
+
+      else
+         VSS.Implementation.String_Configuration.In_Place_Handler.Initialize
+           (Target);
+
+         loop
+            exit when Index > Source'Last;
+
+            Byte := Source (Index);
+
+            case Byte is
+               when 16#00# .. 16#A0# | 16#A2# .. 16#A9#
+                  | 16#AB# .. 16#B9# | 16#BB# .. 16#BE#
+               =>
+                  VSS.Implementation.Strings.Handler (Target).Append
+                    (Target, VSS.Unicode.Code_Point (Byte), Offset);
+
+               when 16#AA# =>
+                  VSS.Implementation.Strings.Handler (Target).Append
+                    (Target, 16#00D7#, Offset);
+
+               when 16#BA# =>
+                  VSS.Implementation.Strings.Handler (Target).Append
+                    (Target, 16#00F7#, Offset);
+
+               when 16#DF# =>
+                  VSS.Implementation.Strings.Handler (Target).Append
+                    (Target, 16#2017#, Offset);
+
+               when 16#FD# =>
+                  VSS.Implementation.Strings.Handler (Target).Append
+                    (Target, 16#200E#, Offset);
+
+               when 16#FE# =>
+                  VSS.Implementation.Strings.Handler (Target).Append
+                    (Target, 16#200F#, Offset);
+
+               when 16#A1# | 16#BF# .. 16#DE# | 16#FB# | 16#FC# | 16#FF# =>
+                  Self.Error := True;
+
+                  exit when Self.Flags (Stop_On_Error);
+
+                  VSS.Implementation.Strings.Handler (Target).Append
+                    (Target, Replacement_Character, Offset);
+
+               when others =>
+                  VSS.Implementation.Strings.Handler (Target).Append
+                    (Target,
+                     VSS.Unicode.Code_Point (Byte) - 16#E0# + 16#05D0#,
+                     Offset);
+            end case;
+
+            Index := Index + 1;
+         end loop;
+      end if;
+   end Decode;
+
+   -------------------
+   -- Error_Message --
+   -------------------
+
+   overriding function Error_Message
+     (Self : ISO88598_Decoder) return VSS.Strings.Virtual_String is
+   begin
+      if Self.Error then
+         return "Unmapped character";
+
+      else
+         return VSS.Strings.Empty_Virtual_String;
+      end if;
+   end Error_Message;
+
+   -------------
+   -- Factory --
+   -------------
+
+   function Factory
+     (Flags : Converter_Flags)
+      return VSS.Strings.Converters.Decoders.Decoder_Access is
+   begin
+      return Result : constant
+        VSS.Strings.Converters.Decoders.Decoder_Access :=
+          new ISO88598_Decoder
+      do
+         declare
+            Self : ISO88598_Decoder renames ISO88598_Decoder (Result.all);
+
+         begin
+            Self.Flags := Flags;
+            Self.Reset_State;
+         end;
+      end return;
+   end Factory;
+
+   ---------------
+   -- Has_Error --
+   ---------------
+
+   overriding function Has_Error (Self : ISO88598_Decoder) return Boolean is
+   begin
+      return Self.Error;
+   end Has_Error;
+
+   -----------------
+   -- Reset_State --
+   -----------------
+
+   overriding procedure Reset_State (Self : in out ISO88598_Decoder) is
+   begin
+      Self.Error := False;
+   end Reset_State;
+
+end VSS.Strings.Converters.Decoders.ISO88598;

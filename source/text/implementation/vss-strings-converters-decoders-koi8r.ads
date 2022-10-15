f5d4f5f7eed7efadfcac551a1c6a211be13df48a
Vadim Godunko <godunko@adacore.com>
2022-10-15 18:47:31 +0400

    V805-018 KOI8-R decoder

---
+++ source/text/implementation/vss-strings-converters-decoders-koi8r.ads
@@ -0,0 +1,35 @@
+--
+--  Copyright (C) 2022, AdaCore
+--
+--  SPDX-License-Identifier: Apache-2.0
+--
+
+--  KOI8-R decoder.
+
+private package VSS.Strings.Converters.Decoders.KOI8R is
+
+   function Factory
+     (Flags : Converter_Flags)
+      return VSS.Strings.Converters.Decoders.Decoder_Access;
+   --  Create KOI8R_Decoder and return it
+
+private
+
+   type KOI8R_Decoder is new Abstract_Decoder with null record;
+
+   overriding procedure Decode
+     (Self        : in out KOI8R_Decoder;
+      Source      : Ada.Streams.Stream_Element_Array;
+      End_Of_Data : Boolean;
+      Target      : out VSS.Implementation.Strings.String_Data);
+
+   overriding function Has_Error
+     (Self : KOI8R_Decoder) return Boolean is (False);
+
+   overriding function Error_Message
+     (Self : KOI8R_Decoder)
+      return VSS.Strings.Virtual_String is (VSS.Strings.Empty_Virtual_String);
+
+   overriding procedure Reset_State (Self : in out KOI8R_Decoder) is null;
+
+end VSS.Strings.Converters.Decoders.KOI8R;

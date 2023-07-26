37d9a72988622e8ee3b893762c214367ad242e49
Vadim Godunko <godunko@adacore.com>
2023-07-26 23:38:12 +0400

    Update test to use new Test_Support package.

--- testsuite/json/test_json_content_handler.adb
+++ testsuite/json/test_json_content_handler.adb
@@ -1,5 +1,5 @@
 --
---  Copyright (C) 2020-2021, AdaCore
+--  Copyright (C) 2020-2023, AdaCore
 --
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
@@ -12,7 +12,11 @@ with Ada.Assertions;
 with VSS.JSON.Content_Handlers;
 with VSS.Strings;
 
+with Test_Support;
+
 procedure Test_JSON_Content_Handler is
+
+   procedure Test_JSON_Content_Handler;
 
    type Test_Content_Handler is
      limited new VSS.JSON.Content_Handlers.JSON_Content_Handler
@@ -192,214 +196,342 @@ procedure Test_JSON_Content_Handler is
       Success := Self.Status;
    end String_Value;
 
-   Handler : aliased Test_Content_Handler;
+   -------------------------------
+   -- Test_JSON_Content_Handler --
+   -------------------------------
+
+   procedure Test_JSON_Content_Handler is
+      Handler : aliased Test_Content_Handler;
+
+      procedure Test_Start_Document;
+      --  Start_Document
+
+      procedure Test_End_Document;
+      --  End_Document
+
+      procedure Test_Start_Array;
+      --  Start_Array
+
+      procedure Test_End_Array;
+      --  End_Array
+
+      procedure Test_Start_Object;
+      --  Start_Object
+
+      procedure Test_End_Object;
+      --  End_Object
+
+      procedure Test_Key_Name;
+      --  Key_Name
+
+      procedure Test_String_Value;
+      --  String_Value
+
+      procedure Test_Number_Value;
+      --  Number_Value
+
+      procedure Test_Boolean_Value;
+      --  Boolean_Value
+
+      procedure Test_Null_Value;
+      --  Null_Value
+
+      procedure Test_Integer_Value;
+      --  Integer_Value
+
+      procedure Test_Float_Value;
+      --  Float_Value
+
+      ------------------------
+      -- Test_Boolean_Value --
+      ------------------------
+
+      procedure Test_Boolean_Value is
+      begin
+         Handler.Status := True;
+         Handler.Boolean_Value (False);
+
+         begin
+            Handler.Status := False;
+            Handler.Boolean_Value (False);
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_Boolean_Value;
+
+      --------------------
+      -- Test_End_Array --
+      --------------------
+
+      procedure Test_End_Array is
+      begin
+         Handler.Status := True;
+         Handler.End_Array;
+
+         begin
+            Handler.Status := False;
+            Handler.End_Array;
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_End_Array;
+
+      -----------------------
+      -- Test_End_Document --
+      -----------------------
+
+      procedure Test_End_Document is
+      begin
+         Handler.Status := True;
+         Handler.End_Document;
+
+         begin
+            Handler.Status := False;
+            Handler.End_Document;
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_End_Document;
+
+      ---------------------
+      -- Test_End_Object --
+      ---------------------
+
+      procedure Test_End_Object is
+      begin
+         Handler.Status := True;
+         Handler.End_Object;
+
+         begin
+            Handler.Status := False;
+            Handler.End_Object;
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_End_Object;
+
+      ----------------------
+      -- Test_Float_Value --
+      ----------------------
+
+      procedure Test_Float_Value is
+      begin
+         Handler.Status := True;
+         Handler.Float_Value (0.0);
+
+         begin
+            Handler.Status := False;
+            Handler.Float_Value (0.0);
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_Float_Value;
+
+      ------------------------
+      -- Test_Integer_Value --
+      ------------------------
+
+      procedure Test_Integer_Value is
+      begin
+         Handler.Status := True;
+         Handler.Integer_Value (0);
+
+         begin
+            Handler.Status := False;
+            Handler.Integer_Value (0);
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_Integer_Value;
+
+      -------------------
+      -- Test_Key_Name --
+      -------------------
+
+      procedure Test_Key_Name is
+      begin
+         Handler.Status := True;
+         Handler.Key_Name (VSS.Strings.Empty_Virtual_String);
+
+         begin
+            Handler.Status := False;
+            Handler.Key_Name (VSS.Strings.Empty_Virtual_String);
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_Key_Name;
+
+      ---------------------
+      -- Test_Null_Value --
+      ---------------------
+
+      procedure Test_Null_Value is
+      begin
+         Handler.Status := True;
+         Handler.Null_Value;
+
+         begin
+            Handler.Status := False;
+            Handler.Null_Value;
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_Null_Value;
+
+      -----------------------
+      -- Test_Number_Value --
+      -----------------------
+
+      procedure Test_Number_Value is
+      begin
+         Handler.Status := True;
+         Handler.Number_Value ((others => <>));
+
+         begin
+            Handler.Status := False;
+            Handler.Number_Value ((others => <>));
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_Number_Value;
+
+      ----------------------
+      -- Test_Start_Array --
+      ----------------------
+
+      procedure Test_Start_Array is
+      begin
+         Handler.Status := True;
+         Handler.Start_Array;
+
+         begin
+            Handler.Status := False;
+            Handler.Start_Array;
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_Start_Array;
+
+      -------------------------
+      -- Test_Start_Document --
+      -------------------------
+
+      procedure Test_Start_Document is
+      begin
+         Handler.Status := True;
+         Handler.Start_Document;
+
+         begin
+            Handler.Status := False;
+            Handler.Start_Document;
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_Start_Document;
+
+      -----------------------
+      -- Test_Start_Object --
+      -----------------------
+
+      procedure Test_Start_Object is
+      begin
+         Handler.Status := True;
+         Handler.Start_Object;
+
+         begin
+            Handler.Status := False;
+            Handler.Start_Object;
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_Start_Object;
+
+      -----------------------
+      -- Test_String_Value --
+      -----------------------
+
+      procedure Test_String_Value is
+      begin
+         Handler.Status := True;
+         Handler.String_Value (VSS.Strings.Empty_Virtual_String);
+
+         begin
+            Handler.Status := False;
+            Handler.String_Value (VSS.Strings.Empty_Virtual_String);
+
+            Test_Support.Fail;
+
+         exception
+            when Ada.Assertions.Assertion_Error =>
+               null;
+         end;
+      end Test_String_Value;
+
+   begin
+      Test_Support.Run_Testcase (Test_Start_Document'Access, "Start_Document");
+      Test_Support.Run_Testcase (Test_End_Document'Access, "End_Document");
+      Test_Support.Run_Testcase (Test_Start_Array'Access, "Start_Array");
+      Test_Support.Run_Testcase (Test_End_Array'Access, "End_Array");
+      Test_Support.Run_Testcase (Test_Start_Object'Access, "Start_Object");
+      Test_Support.Run_Testcase (Test_End_Object'Access, "End_Object");
+      Test_Support.Run_Testcase (Test_Key_Name'Access, "Key_Name");
+      Test_Support.Run_Testcase (Test_String_Value'Access, "String_Value");
+      Test_Support.Run_Testcase (Test_Number_Value'Access, "Number_Value");
+      Test_Support.Run_Testcase (Test_Boolean_Value'Access, "Boolean_Value");
+      Test_Support.Run_Testcase (Test_Null_Value'Access, "Null_Value");
+      Test_Support.Run_Testcase (Test_Integer_Value'Access, "Integer_Value");
+      Test_Support.Run_Testcase (Test_Float_Value'Access, "Float_Value");
+   end Test_JSON_Content_Handler;
 
 begin
-   --  Start_Document
-
-   Handler.Status := True;
-   Handler.Start_Document;
-
-   begin
-      Handler.Status := False;
-      Handler.Start_Document;
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  End_Document
-
-   Handler.Status := True;
-   Handler.End_Document;
-
-   begin
-      Handler.Status := False;
-      Handler.End_Document;
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  Start_Array
-
-   Handler.Status := True;
-   Handler.Start_Array;
-
-   begin
-      Handler.Status := False;
-      Handler.Start_Array;
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  End_Array
-
-   Handler.Status := True;
-   Handler.End_Array;
-
-   begin
-      Handler.Status := False;
-      Handler.End_Array;
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  Start_Object
-
-   Handler.Status := True;
-   Handler.Start_Object;
-
-   begin
-      Handler.Status := False;
-      Handler.Start_Object;
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  End_Object
-
-   Handler.Status := True;
-   Handler.End_Object;
-
-   begin
-      Handler.Status := False;
-      Handler.End_Object;
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  Key_Name
-
-   Handler.Status := True;
-   Handler.Key_Name (VSS.Strings.Empty_Virtual_String);
-
-   begin
-      Handler.Status := False;
-      Handler.Key_Name (VSS.Strings.Empty_Virtual_String);
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  String_Value
-
-   Handler.Status := True;
-   Handler.String_Value (VSS.Strings.Empty_Virtual_String);
-
-   begin
-      Handler.Status := False;
-      Handler.String_Value (VSS.Strings.Empty_Virtual_String);
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  Number_Value
-
-   Handler.Status := True;
-   Handler.Number_Value ((others => <>));
-
-   begin
-      Handler.Status := False;
-      Handler.Number_Value ((others => <>));
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  Boolean_Value
-
-   Handler.Status := True;
-   Handler.Boolean_Value (False);
-
-   begin
-      Handler.Status := False;
-      Handler.Boolean_Value (False);
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  Null_Value
-
-   Handler.Status := True;
-   Handler.Null_Value;
-
-   begin
-      Handler.Status := False;
-      Handler.Null_Value;
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  Integer_Value
-
-   Handler.Status := True;
-   Handler.Integer_Value (0);
-
-   begin
-      Handler.Status := False;
-      Handler.Integer_Value (0);
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
-
-   --  Float_Value
-
-   Handler.Status := True;
-   Handler.Float_Value (0.0);
-
-   begin
-      Handler.Status := False;
-      Handler.Float_Value (0.0);
-
-      raise Program_Error;
-
-   exception
-      when Ada.Assertions.Assertion_Error =>
-         null;
-   end;
+   Test_Support.Run_Testsuite
+     (Test_JSON_Content_Handler'Access, "JSON_Content_Handler");
 end Test_JSON_Content_Handler;

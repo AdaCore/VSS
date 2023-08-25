021c1bdfe2e8dc393f07185425af22b549c38806
Vadim Godunko <godunko@adacore.com>
2023-08-25 11:51:11 +0400

    Automated test for "*" operator.

---
+++ testsuite/text/test_string-test_asterisk_character.adb
@@ -0,0 +1,29 @@
+--
+--  Copyright (C) 2020-2023, AdaCore
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+separate (Test_String)
+procedure Test_Asterisk_Character is
+
+   use type VSS.Strings.Virtual_String;
+
+begin
+   declare
+      R : constant VSS.Strings.Virtual_String := 0 * 'A';
+
+   begin
+      Test_Support.Assert (R.Is_Empty);
+      --  Test_Support.Assert (not R.Is_Null);
+      --  XXX Should it be non-null string?
+   end;
+
+   declare
+      R : constant VSS.Strings.Virtual_String := 5 * 'B';
+
+   begin
+      Test_Support.Assert (not R.Is_Empty);
+      Test_Support.Assert (R = "BBBBB");
+   end;
+end Test_Asterisk_Character;

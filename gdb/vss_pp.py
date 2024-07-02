ae51919c7fa66976f6df38e64d63120ca9cf96e6
Vadim Godunko <godunko@adacore.com>
2024-07-02 22:49:34 +0400

    Update GDB pretty-printer

--- gdb/vss_pp.py
+++ gdb/vss_pp.py
@@ -1,31 +1,33 @@
+from gnatdbg.tagged import reinterpret_tagged
 
-def decode_utf8(val):
-    result = ''
+
+def decode_utf8(bytes, size):
+    result = ""
     i = 0
 
-    while i < val["size"]:
-        if val["storage"][i] <= 0x7F:
-            c = val["storage"][i]
+    while i < size:
+        if bytes[i] <= 0x7F:
+            c = bytes[i]
             i = i + 1
 
-        elif val["storage"][i] <= 0xDF:
-            u1 = (val["storage"][i] & 0x1F) << 6
-            u2 = val["storage"][i + 1] & 0x3F
+        elif bytes[i] <= 0xDF:
+            u1 = (bytes[i] & 0x1F) << 6
+            u2 = bytes[i + 1] & 0x3F
             c = u1 | u2
             i = i + 2
 
-        elif val["storage"][i] <= 0xEF:
-            u1 = (val["storage"][i] & 0x1F) << 12
-            u2 = (val["storage"][i + 1] & 0x3F) << 6
-            u3 = val["storage"][i + 2] & 0x3F
+        elif bytes[i] <= 0xEF:
+            u1 = (bytes[i] & 0x1F) << 12
+            u2 = (bytes[i + 1] & 0x3F) << 6
+            u3 = bytes[i + 2] & 0x3F
             c = u1 | u2 | u3
             i = i + 3
 
-        elif val["storage"][i] <= 0xF4:
-            u1 = (val["storage"][i] & 0x1F) << 18
-            u2 = (val["storage"][i + 1] & 0x3F) << 12
-            u3 = (val["storage"][i + 2] & 0x3F) << 6
-            u4 = val["storage"][i + 3] & 0x3F
+        elif bytes[i] <= 0xF4:
+            u1 = (bytes[i] & 0x1F) << 18
+            u2 = (bytes[i + 1] & 0x3F) << 12
+            u3 = (bytes[i + 2] & 0x3F) << 6
+            u4 = bytes[i + 3] & 0x3F
             c = u1 | u2 | u3 | u4
             i = i + 4
 
@@ -33,42 +35,53 @@ def decode_utf8(val):
 
     return result
 
-def utf8_pp(val):
-    data_type = gdb.lookup_type("vss.implementation.utf8_string_handlers.utf8_string_data").pointer()
-    data = val.cast(data_type).dereference()
-
-    return decode_utf8(data)
-
-def utf8ip_pp(val):
-    data_type = gdb.lookup_type("vss.implementation.utf8_string_handlers.utf8_in_place_data")
-    data = val.cast(data_type)
-
-    return decode_utf8(data)
 
 class Virtual_String_Printer:
     def __init__(self, val):
         self.val = val
 
     def to_string(self):
-        data = self.val["data"]
+        text_type = gdb.lookup_type(
+            "vss.implementation.text_handlers.abstract_text_handler"
+        )
+        utf8static_type = gdb.lookup_type(
+            "vss.implementation.text_handlers.utf8.static.static_utf8_handler"
+        )
+        utf8dynamic_type = gdb.lookup_type(
+            "vss.implementation.text_handlers.utf8.dynamic.dynamic_utf8_handler"
+        )
 
-        if data["in_place"]:
-            return utf8ip_pp(data["storage"])
+        storage = self.val["data"]["storage"]
+
+        if all(byte == 0 for byte in storage.bytes):
+            # "null" string
+
+            return None
+
+        text = reinterpret_tagged(storage.cast(text_type))
+
+        if text.type == utf8static_type:
+            # GDB is unable to resolve "storage" component of the record,
+            # so skip first implicit component of 8 bytes and decode.
+            return decode_utf8(text.bytes[8:], text["size"])
+
+        elif text.type == utf8dynamic_type:
+            data = text["pointer"].dereference()
+            return decode_utf8(data["storage"].bytes, data["size"])
 
         else:
-            if data["handler"] == 0:
-                return None
+            print("<UNKNOWN TYPE>")
 
-            else:
-                if data["handler"] == gdb.parse_and_eval("vss.implementation.utf8_string_handlers.global_utf8_string_handler'address"):
-                    return utf8_pp(data["pointer"])
         return None
 
+
 #    def display_hint(self):
-#        return 'string'
+#        return "string"
+
 
 def vss_pp_func(val):
-    if str(val.type) == 'vss.strings.virtual_string':
+    if str(val.type) == "vss.strings.virtual_string":
         return Virtual_String_Printer(val)
+
 
 gdb.pretty_printers.append(vss_pp_func)

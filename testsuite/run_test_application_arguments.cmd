9a5c3f539ff674b5058a047de23d91480a3adace
Vadim Godunko <godunko@adacore.com>
2024-07-05 10:54:09 +0000

    Force CHCP 65001 to process UTF-8 encoding correctly

--- testsuite/run_test_application_arguments.cmd
+++ testsuite/run_test_application_arguments.cmd
@@ -1,4 +1,5 @@
 ::  Run test driver executable and pass necessary arguments
 ::  It is workaround of failure to process command line arguments with Unicode
 ::  characters by Makefile/bash on GitHub Actions.
+chcp 65001
 .objs\tests\test_application_arguments.exe hello привет გამარჯობა 👋

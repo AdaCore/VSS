d0727135d1644fe54b435ff469e7fe95b2257e09
Vadim Godunko <godunko@adacore.com>
2025-04-23 12:09:46 +0400

    Debug utilities.

---
+++ source/xml/templates/implementation/vss-xml-implementation-template_programs-debug.ads
@@ -0,0 +1,11 @@
+--
+--  Copyright (C) 2025, AdaCore
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+package VSS.XML.Implementation.Template_Programs.Debug is
+
+   procedure Dump_Program (Program : Instruction_Vectors.Vector);
+
+end VSS.XML.Implementation.Template_Programs.Debug;

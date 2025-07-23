2125ff2c82bc00036ec0ab8cca801b6f4d81d3a9
Vadim Godunko <godunko@adacore.com>
2025-07-23 18:40:47 +0400

    Testcase to check mutate of shared text data by `Delete`

--- testsuite/text/test_string-test_delete.adb
+++ testsuite/text/test_string-test_delete.adb
@@ -44,4 +44,34 @@ begin
       Test_Support.Assert (not J2.Is_Valid);
       Test_Support.Assert (J3.Character_Index = 6);
    end;
+
+   --  VSS#278
+   --
+   --  Check that `Delete` mutate shared text data, thus other objects are not
+   --  modified.
+
+   declare
+      S          : VSS.Strings.Virtual_String :=
+        "      Ada_With_Private_Absent,";
+      SB         : constant VSS.Strings.Virtual_String := S;
+      S_Iterator : VSS.Strings.Character_Iterators.Character_Iterator :=
+        S.At_First_Character;
+      E_Iterator : VSS.Strings.Character_Iterators.Character_Iterator :=
+        S.At_Last_Character;
+
+   begin
+      Test_Support.Assert (S_Iterator.Forward);
+      Test_Support.Assert (S_Iterator.Forward);
+      Test_Support.Assert (S_Iterator.Forward);
+      Test_Support.Assert (S_Iterator.Forward);
+      Test_Support.Assert (S_Iterator.Forward);
+      Test_Support.Assert (S_Iterator.Forward);
+
+      Test_Support.Assert (E_Iterator.Backward);
+
+      S.Delete (S_Iterator, E_Iterator);
+
+      Test_Support.Assert (SB = "      Ada_With_Private_Absent,");
+      Test_Support.Assert (S = "      ,");
+   end;
 end Test_Delete;

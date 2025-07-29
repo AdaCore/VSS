f2b2142b511b11246fd566866066b5bdb528edb8
Vadim Godunko <godunko@adacore.com>
2025-07-29 09:38:33 +0400

    Remove text support.

--- testsuite/gdbpp/test.sh
+++
@@ -1,7 +0,0 @@
-gprbuild -q -P test.gpr
-if [ "$OS" = "Windows_NT" ]; then
-  gdb --batch --command=gdbinit.windows --directory=$VSS_GDBPP .objs/main
-else
-  export LC_ALL=en_US.UTF-8
-  gdb --batch --command=gdbinit.posix --directory=$VSS_GDBPP .objs/main
-fi
--- testsuite/text/performance/U816-022/README.md
+++
@@ -1,4 +0,0 @@
-# Performance test of the `Virtual_String`.`Slice` operation
-
-It is manual test, last time it executes 37,903,618 instructions.
-Use `make clean run` to build and run test.
